a54370e59ddc42ef5edf01e74db475d89f7a0379
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2020-06-13 20:48:24 +0200

    Conversion of Naive Bayes algorithm to S4 object.

--- R/LearnerMain.R
+++ R/LearnerMain.R
@@ -4,6 +4,7 @@
 #' @include LearnerS4GLM.R
 #' @include LearnerS4GLMnet.R
 #' @include LearnerS4KNN.R
+#' @include LearnerS4NaiveBayes.R
 #' @include LearnerS4Ranger.R
 #' @include LearnerS4RFSRC.R
 NULL
