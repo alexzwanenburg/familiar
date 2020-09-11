9cfbb5725a5416acfe54158e1222fe9d65592ecd
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2020-09-11 16:24:40 +0200

    Create test-plot_confusion_matrix.R
    Added unit test for confusion matrix plots.

---
+++ tests/testthat/test-plot_confusion_matrix.R
@@ -0,0 +1,31 @@
+familiar:::test_plots(plot_function=familiar:::plot_confusion_matrix,
+                      data_element="confusion_matrix",
+                      outcome_type_available=c("binomial", "multinomial"))
+
+# Test confusion matrix without transparent cells.
+familiar:::test_plots(plot_function=familiar:::plot_confusion_matrix,
+                      data_element="confusion_matrix",
+                      outcome_type_available=c("binomial", "multinomial"),
+                      test_specific_config=TRUE,
+                      plot_args=list("show_alpha"="none"))
+
+# Test by confusion matrix with transparency by class.
+familiar:::test_plots(plot_function=familiar:::plot_confusion_matrix,
+                      data_element="confusion_matrix",
+                      outcome_type_available=c("binomial", "multinomial"),
+                      test_specific_config=TRUE,
+                      plot_args=list("show_alpha"="by_class"))
+
+# Test by confusion matrix with transparency by the entire plot.
+familiar:::test_plots(plot_function=familiar:::plot_confusion_matrix,
+                      data_element="confusion_matrix",
+                      outcome_type_available=c("binomial", "multinomial"),
+                      test_specific_config=TRUE,
+                      plot_args=list("show_alpha"="by_figure"))
+
+# Test by confusion matrix with transparency by the entire plot.
+familiar:::test_plots(plot_function=familiar:::plot_confusion_matrix,
+                      data_element="confusion_matrix",
+                      outcome_type_available=c("binomial", "multinomial"),
+                      test_specific_config=TRUE,
+                      plot_args=list("show_alpha"="by_all"))
