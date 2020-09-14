4d70025990586d6447faa8c9a20a5750854431b6
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2020-09-14 15:28:16 +0200

    Update test-plot_auc_curves.R
    Added plot ordering test.

--- tests/testthat/test-plot_auc_curves.R
+++ tests/testthat/test-plot_auc_curves.R
@@ -29,3 +29,15 @@ familiar:::test_plots(plot_function=familiar:::plot_auc_roc_curve,
                       outcome_type_available=c("binomial", "multinomial"),
                       aggregate_ci=FALSE,
                       test_specific_config=TRUE)
+
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_auc_roc_curve,
+                              data_element="auc_data",
+                              outcome_type_available=c("binomial", "multinomial"))
+
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_auc_roc_curve,
+                              data_element="auc_data",
+                              outcome_type_available=c("binomial", "multinomial"),
+                              plot_args=list("facet_by"=c("fs_method", "learner"),
+                                             "color_by"=c("data_set", "pos_class")))
