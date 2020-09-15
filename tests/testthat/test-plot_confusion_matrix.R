c72c6495675ff29713fef571269b681025bc7ebe
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2020-09-15 11:10:20 +0200

    Update test-plot_confusion_matrix.R
    Tested plot ordering.

--- tests/testthat/test-plot_confusion_matrix.R
+++ tests/testthat/test-plot_confusion_matrix.R
@@ -29,3 +29,14 @@ familiar:::test_plots(plot_function=familiar:::plot_confusion_matrix,
                       outcome_type_available=c("binomial", "multinomial"),
                       test_specific_config=TRUE,
                       plot_args=list("show_alpha"="by_all"))
+
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_confusion_matrix,
+                              data_element="confusion_matrix",
+                              outcome_type_available=c("binomial", "multinomial"))
+
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_confusion_matrix,
+                              data_element="confusion_matrix",
+                              outcome_type_available=c("binomial", "multinomial"),
+                              plot_args=list("facet_by"=c("data_set", "learner", "fs_method")))
