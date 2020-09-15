21818dfce9172550bf158589d22365cb8c5ac8f9
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2020-09-15 11:57:47 +0200

    Update test-plot_decision_curve.R
    Extended the plot_decision_curve unit test.

--- tests/testthat/test-plot_decision_curve.R
+++ tests/testthat/test-plot_decision_curve.R
@@ -30,3 +30,29 @@ familiar:::test_plots(plot_function=familiar:::plot_decision_curve,
                       aggregate_ci=FALSE,
                       test_specific_config=TRUE)
 
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_decision_curve,
+                              data_element="decision_curve_analyis",
+                              compute_ensemble_ci=FALSE,
+                              outcome_type_available=c("binomial", "multinomial", "survival"))
+
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_decision_curve,
+                              data_element="decision_curve_analyis",
+                              outcome_type_available=c("binomial"),
+                              plot_args=list("facet_by"=c("learner", "fs_method"),
+                                             "color_by"="data_set"))
+
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_decision_curve,
+                              data_element="decision_curve_analyis",
+                              outcome_type_available=c("multinomial"),
+                              plot_args=list("facet_by"=c("learner", "fs_method"),
+                                             "color_by"=c("data_set", "pos_class")))
+
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_decision_curve,
+                              data_element="decision_curve_analyis",
+                              outcome_type_available=c("survival"),
+                              plot_args=list("facet_by"=c("learner", "fs_method"),
+                                             "color_by"=c("data_set")))
