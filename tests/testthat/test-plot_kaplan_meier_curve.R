829f23c538a341679d5e35b3a59a5fa97e3c5f9c
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2020-09-23 22:02:48 +0200

    Initial restructuring and unit test for kaplan-meier plots.

---
+++ tests/testthat/test-plot_kaplan_meier_curve.R
@@ -0,0 +1,19 @@
+# Generic test
+familiar:::test_plots(plot_function=familiar:::plot_kaplan_meier,
+                      except_one_feature = TRUE,
+                      outcome_type_available=c("survival"),
+                      data_element="kaplan_meier_data",
+                      debug=TRUE)
+
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_kaplan_meier,
+                              data_element="kaplan_meier_data",
+                              outcome_type_available=c("survival"))
+
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_kaplan_meier,
+                              data_element="kaplan_meier_data",
+                              stratification_ensemble_method="mean_threshold",
+                              outcome_type_available=c("survival"),
+                              plot_args=list("facet_by"=c("learner", "fs_method", "data_set"),
+                                             "color_by"="risk_group"), debug=TRUE)
