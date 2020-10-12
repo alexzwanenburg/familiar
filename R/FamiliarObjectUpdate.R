70040f2486d8b4819565b097e505b34bb9c16fad
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2020-10-12 21:41:20 +0200

    Added basic update routines to facilitate backward compatibility.

---
+++ R/FamiliarObjectUpdate.R
@@ -0,0 +1,107 @@
+#' @include FamiliarS4Generics.R
+#' @include FamiliarS4Classes.R
+NULL
+
+#'@title Update familiar S4 objects to the most recent version.
+#'
+#'@description Creates `familiarEnsemble` a object from `familiarModel` objects.
+#'
+#'@param object A `familiarModel`, a `familiarEnsemble`, a `familiarData` or `familiarCollection` object.
+#'@param ... Unused arguments.
+#'
+#'@return An up-to-date version of the respective S4 object.
+#'@exportMethod update_object
+#'@md
+#'@rdname update_object-methods
+setGeneric("update_object", function(object, ...) standardGeneric("update_object"))
+
+
+#'@rdname update_object-methods
+setMethod("update_object", signature(object="familiarModel"),
+          function(object, ...){
+            
+            if(object@familiar_version < "0.0.0.54"){
+              
+              # Rename req_feature_cols to required_features
+              attr(object, "required_features") <- attr(object, "req_feature_cols")
+              attr(object, "req_feature_cols") <- NULL
+              
+              # Rename important_features to model_features
+              attr(object, "model_features") <- attr(object, "important_features")
+              attr(object, "important_features") <- NULL
+              
+              # Introduce missing novelty_features slot by copying
+              # model_features.
+              attr(object, "novelty_features") <- attr(object, "model_features")
+              
+              # Remove signature altogether.
+              attr(object, "signature") <- NULL
+            }
+            
+            if(!methods::validObject(object)) stop("Could not update the familiarModel object to the most recent definition.")
+            
+            return(object)
+          })
+
+
+#'@rdname update_object-methods
+setMethod("update_object", signature(object="familiarEnsemble"),
+          function(object, ...){
+            
+            if(object@familiar_version < "0.0.0.54"){
+              
+              # Rename req_feature_cols to required_features
+              attr(object, "required_features") <- attr(object, "req_feature_cols")
+              attr(object, "req_feature_cols") <- NULL
+              
+              # Rename important_features to model_features
+              attr(object, "model_features") <- attr(object, "important_features")
+              attr(object, "important_features") <- NULL
+            }
+            
+            if(!methods::validObject(object)) stop("Could not update the familiarEnsemble object to the most recent definition.")
+            
+            return(object)
+          })
+
+
+#'@rdname update_object-methods
+setMethod("update_object", signature(object="familiarData"),
+          function(object, ...){
+            
+            if(object@familiar_version < "0.0.0.54"){
+              
+              # Rename req_feature_cols to required_features
+              attr(object, "required_features") <- attr(object, "req_feature_cols")
+              attr(object, "req_feature_cols") <- NULL
+              
+              # Rename important_features to model_features
+              attr(object, "model_features") <- attr(object, "important_features")
+              attr(object, "important_features") <- NULL
+            }
+            
+            if(!methods::validObject(object)) stop("Could not update the familiarData object to the most recent definition.")
+            
+            return(object)
+          })
+
+
+#'@rdname update_object-methods
+setMethod("update_object", signature(object="familiarCollection"),
+          function(object, ...){
+            
+            if(object@familiar_version < "0.0.0.54"){
+              
+              # Rename req_feature_cols to required_features
+              attr(object, "required_features") <- attr(object, "req_feature_cols")
+              attr(object, "req_feature_cols") <- NULL
+              
+              # Rename important_features to model_features
+              attr(object, "model_features") <- attr(object, "important_features")
+              attr(object, "important_features") <- NULL
+            }
+            
+            if(!methods::validObject(object)) stop("Could not update the familiarCollection object to the most recent definition.")
+            
+            return(object)
+          })
