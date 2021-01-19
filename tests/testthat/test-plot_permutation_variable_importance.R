32102d919c30fe4c04c51b8976274708a01d3ec4
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2021-01-19 21:19:09 +0100

    Updated unit test for permutation variable importance plots.

--- tests/testthat/test-plot_permutation_variable_importance.R
+++ tests/testthat/test-plot_permutation_variable_importance.R
@@ -1,54 +1,71 @@
 # Don't perform any further tests on CRAN due to time of running the complete test.
 testthat::skip_on_cran()
 
+debug_flag <- FALSE
+
 # Generic test.
 familiar:::test_plots(plot_function=familiar:::plot_permutation_variable_importance,
                       data_element="permutation_vimp",
                       outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
                       estimation_type="point",
-                      debug=TRUE)
+                      debug=debug_flag)
 
 # Point line confidence intervals.
 familiar:::test_plot_ordering(plot_function=familiar:::plot_permutation_variable_importance,
                               data_element="permutation_vimp",
-                              compute_ensemble_ci=TRUE,
+                              estimation_type="bci",
                               confidence_level=0.50,
                               metric=c("auc_roc", "accuracy"),
                               outcome_type_available=c("multinomial"),
-                              plot_args=list("conf_int_style"="point_line"))
+                              plot_args=list("conf_int_style"="point_line"),
+                              debug=debug_flag)
 
 # Line style confidence intervals.
 familiar:::test_plot_ordering(plot_function=familiar:::plot_permutation_variable_importance,
                               data_element="permutation_vimp",
-                              compute_ensemble_ci=TRUE,
+                              estimation_type="bci",
                               confidence_level=0.50,
                               metric=c("auc_roc", "accuracy"),
                               outcome_type_available=c("multinomial"),
-                              plot_args=list("conf_int_style"="line"))
+                              plot_args=list("conf_int_style"="line"),
+                              debug=debug_flag)
 
 # Bar + line style confidence intervals.
 familiar:::test_plot_ordering(plot_function=familiar:::plot_permutation_variable_importance,
                               data_element="permutation_vimp",
-                              compute_ensemble_ci=TRUE,
+                              estimation_type="bci",
                               confidence_level=0.50,
                               metric=c("auc_roc", "accuracy"),
                               outcome_type_available=c("multinomial"),
-                              plot_args=list("conf_int_style"="bar_line"))
+                              plot_args=list("conf_int_style"="bar_line"),
+                              debug=debug_flag)
 
 # No confidence intervals.
 familiar:::test_plot_ordering(plot_function=familiar:::plot_permutation_variable_importance,
                               data_element="permutation_vimp",
-                              compute_ensemble_ci=TRUE,
+                              estimation_type="bci",
                               confidence_level=0.50,
                               metric=c("auc_roc", "accuracy"),
                               outcome_type_available=c("multinomial"),
-                              plot_args=list("conf_int_style"="none"))
+                              plot_args=list("conf_int_style"="none"),
+                              debug=debug_flag)
 
 
 familiar:::test_plot_ordering(plot_function=familiar:::plot_permutation_variable_importance,
                               data_element="permutation_vimp",
-                              compute_ensemble_ci=FALSE,
+                              estimation_type="point",
                               metric=c("auc_roc", "accuracy"),
                               outcome_type_available=c("multinomial"),
                               plot_args=list("facet_by"=c("data_set", "learner", "fs_method"),
-                                             "color_by"=c("metric", "similarity_threshold")))
+                                             "color_by"=c("metric", "similarity_threshold")),
+                              debug=debug_flag)
+
+# Different clustering method.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_permutation_variable_importance,
+                              data_element="permutation_vimp",
+                              feature_cluster_method="pam",
+                              feature_cluster_cut_method = "silhouette",
+                              estimation_type="point",
+                              metric=c("auc_roc", "accuracy"),
+                              outcome_type_available=c("multinomial"),
+                              debug=debug_flag)
