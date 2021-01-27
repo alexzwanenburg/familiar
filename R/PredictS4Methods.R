400a5af5db91906a6add863bafd5f44b7091beff
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2021-01-27 20:33:18 +0100

    Updated .predict to handle different type arguments more flexibly.

--- R/PredictS4Methods.R
+++ R/PredictS4Methods.R
@@ -2,10 +2,14 @@
 #' @include FamiliarS4Classes.R
 NULL
 
+.get_available_prediction_type_arguments <- function(){
+  return(c("novelty", "default", "survival_probability", "risk_stratification"))
+}
+
 
 #####.predict (familiarEnsemble)#####
 setMethod(".predict", signature(object="familiarEnsemble"),
-          function(object, data, allow_recalibration=TRUE, is_pre_processed=FALSE, time=NULL, type=NULL, dir_path=NULL, ensemble_method="median", novelty=FALSE, ...) {
+          function(object, data, allow_recalibration=TRUE, is_pre_processed=FALSE, time=NULL, type="default", dir_path=NULL, ensemble_method="median", ...) {
             # Predict function for a model ensemble. This will always return
             # ensemble information, and not details corresponding to the
             # individual models.
@@ -23,7 +27,7 @@ setMethod(".predict", signature(object="familiarEnsemble"),
                                      is_pre_processed=is_pre_processed,
                                      time=time,
                                      type=type,
-                                     novelty=novelty)
+                                     ...)
               
             } else {
               # Process data
@@ -31,82 +35,142 @@ setMethod(".predict", signature(object="familiarEnsemble"),
                                          data=data,
                                          is_pre_processed=is_pre_processed,
                                          stop_at="clustering",
-                                         keep_novelty=novelty)
+                                         keep_novelty="novelty" %in% type)
               
-              # Generate a placehold table.
+              # Generate a placeholder table.
               predict_list <- list(get_placeholder_prediction_table(object=object, data=data))
             }
             
             
             ##### Ensemble predictions #####
             # Generate ensemble predictions
-            prediction_data <- ensemble_prediction(object=object,
-                                                   prediction_data=data.table::rbindlist(predict_list),
-                                                   ensemble_method=ensemble_method)
-            
+            if(all(type %in% .get_available_prediction_type_arguments())){
+              prediction_data <- ensemble_prediction(object=object,
+                                                     prediction_data=data.table::rbindlist(predict_list),
+                                                     ensemble_method=ensemble_method)
+            }
+
             # Return data
             return(prediction_data)
           })
 
 
-
 #####.predict (familiarModel)#####
 setMethod(".predict", signature(object="familiarModel"),
-          function(object, data, allow_recalibration=TRUE, is_pre_processed=FALSE, time=NULL, type=NULL, novelty=FALSE, ...) {
+          function(object, data, allow_recalibration=TRUE, is_pre_processed=FALSE, time=NULL, type="default", ...) {
             
             # Prepare input data
             data <- process_input_data(object=object,
                                        data=data,
                                        is_pre_processed=is_pre_processed,
                                        stop_at="clustering",
-                                       keep_novelty=novelty)
+                                       keep_novelty="novelty" %in% type)
             
-            # Add novelty.
-            if(novelty){
-              # Determine novelty
-              novelty_values <- .predict_novelty(object=object,
-                                                 data=data)
+            # Set empty prediction table that can be updated
+            prediction_table <- NULL
+            
+            
+            ##### User specified type ##########################################
+            if(any(!type %in% .get_available_prediction_type_arguments())){
+              # Select the first non-standard type.
+              type <- setdiff(type, .get_available_prediction_type_arguments())[1]
               
-              # Keep only model features in data.
+              # Select only features used by the model.
+              data <- select_features(data=data,
+                                      features=features_after_clustering(features=object@model_features,
+                                                                         feature_info_list=object@feature_info))
+              
+              # Call predict from ..predict method with the given type and the
+              # unspecified extra arguments in ... .
+              prediction_element <- ..predict(object=object,
+                                              data=data,
+                                              time=time,
+                                              type=type,
+                                              ...)
+              return(prediction_element)
+            }
+            
+            ##### Novelty ######################################################
+            if("novelty" %in% type){
+              
+              # Predict instance novelty.
+              prediction_table <- .predict_novelty(object=object,
+                                                   data=data)
+              
+              # Keep only model features in data for the remaining analysis.
               data <- select_features(data=data,
                                       features=features_after_clustering(features=object@model_features,
                                                                          feature_info_list=object@feature_info))
             }
             
-            if(is.null(type)){
+            ##### Default response #############################################
+            if("default" %in% type){
               
               # Predict using the model and the standard type.
-              prediction_table <- ..predict(object=object,
-                                            data=data,
-                                            time=time)
+              temp_prediction_table <- ..predict(object=object,
+                                                 data=data,
+                                                 time=time)
               
+              # Recalibrate the results.
               if(allow_recalibration){
-                # Recalibrate the results.
-                prediction_table <- learner.apply_calibration(object=object,
-                                                              predictions=prediction_table)
+                temp_prediction_table <- learner.apply_calibration(object=object,
+                                                                   predictions=temp_prediction_table)
               }
               
-            } else if(type == "survival_probability") {
-              # Prediction survival probabilities,
-              prediction_table <- ..predict_survival_probability(object=object,
-                                                                 data=data,
-                                                                 time=time)
-              
-            } else {
-              # Predict using a user-specified type.
-              prediction_table <- ..predict(object=object,
-                                            data=data,
-                                            time=time,
-                                            type=type)
+              # Add new columns to existing prediction table, if necessary.
+              if(is.null(prediction_table)){
+                prediction_table <- temp_prediction_table
+                
+              } else if(!is_empty(temp_prediction_table)){
+                prediction_table <- merge(x=prediction_table,
+                                          y=temp_prediction_table,
+                                          by=get_non_feature_columns(x=object))
+              }
             }
             
-            # Order output columns.
-            data.table::setcolorder(prediction_table,
-                                    neworder=colnames(get_placeholder_prediction_table(object=object, data=data)))
+            ##### Survival probability #########################################
+            if("survival_probability" %in% type) {
+              # Prediction survival probabilities,
+              temp_prediction_table <- ..predict_survival_probability(object=object,
+                                                                      data=data,
+                                                                      time=time)
+              
+              # Add new columns to existing prediction table, if necessary.
+              if(is.null(prediction_table)){
+                prediction_table <- temp_prediction_table
+                
+              } else if(!is_empty(temp_prediction_table)){
+                # Merge with the prediction table
+                prediction_table <- merge(x=prediction_table,
+                                          y=temp_prediction_table,
+                                          by=get_non_feature_columns(x=object))
+              }
+            }
             
-            # Add novelty to the prediction table
-            if(novelty) prediction_table[, "novelty":=novelty_values]
+            ##### Risk stratification ##########################################
+            if("risk_stratification" %in% type){
+              # Prediction survival probabilities,
+              temp_prediction_table <- ..predict_risk_stratification(object=object,
+                                                                     data=data,
+                                                                     time=time)
+              
+              # Add new columns to existing prediction table, if necessary.
+              if(is.null(prediction_table)){
+                prediction_table <- temp_prediction_table
+                
+              } else if(!is_empty(temp_prediction_table)){
+                prediction_table <- merge(x=prediction_table,
+                                          y=temp_prediction_table,
+                                          by=get_non_feature_columns(x=object))
+              }
+            }
             
+            if(!is_empty(prediction_table)){
+              # Order output columns.
+              data.table::setcolorder(prediction_table,
+                                      neworder=colnames(get_placeholder_prediction_table(object=object, data=data)))
+            }  
+
             return(prediction_table)  
           })
 
@@ -136,11 +200,16 @@ setMethod(".predict_novelty", signature(object="familiarModel"),
                                        stop_at="clustering",
                                        keep_novelty=TRUE)
             
+            # Get a placeholder prediction table.
+            prediction_table <- get_placeholder_prediction_table(object=object,
+                                                                 data=data,
+                                                                 type="novelty")
+            
             # Return NA if there is no novelty detector.
-            if(is.null(object@novelty_detector)) return(rep(NA_real_, times=nrow(data@data)))
+            if(is.null(object@novelty_detector)) return(prediction_table)
             
             # Return empty if there is no data.
-            if(is_empty(data)) return(numeric(0))
+            if(is_empty(data)) return(prediction_table)
             
             # Find and replace ordered features.
             ordered_features <- colnames(data@data)[sapply(data@data, is.ordered)]
@@ -150,8 +219,14 @@ setMethod(".predict_novelty", signature(object="familiarModel"),
                                                      ordered=FALSE)
             }
             
-            return(predict(object=object@novelty_detector,
-                           newdata=data@data))
+            # Find novelty values.
+            novelty_values <- predict(object=object@novelty_detector,
+                                      newdata=data@data)
+            
+            # Store the novelty values in the table.
+            prediction_table[, "novelty":=novelty_values]
+            
+            return(prediction_table)
           })
 
 
