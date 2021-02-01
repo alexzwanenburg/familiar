c8d33dbf0997b671a4233b2cd2317e927579529c
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2021-02-01 14:27:37 +0100

    Added unit test for prediction data generation and export.

---
+++ tests/testthat/test-export_prediction_data.R
@@ -0,0 +1,11 @@
+# Don't perform any further tests on CRAN due to time of running the complete test.
+testthat::skip_on_cran()
+
+debug_flag <- TRUE
+
+familiar:::test_export(export_function=familiar:::export_prediction_data,
+                       data_element="prediction_data",
+                       outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
+                       detail_level="ensemble",
+                       create_novelty_detector=TRUE,
+                       debug=debug_flag)
