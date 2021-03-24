548321bb7f3183bf41cd69910143046486696ff3
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2021-03-24 10:16:47 +0100

    Update test call for stratification plot unit test.

--- tests/testthat/test-plot_kaplan_meier_curve.R
+++ tests/testthat/test-plot_kaplan_meier_curve.R
@@ -6,18 +6,18 @@ debug_flag <- FALSE
 # Generic test
 familiar:::test_plots(plot_function=familiar:::plot_kaplan_meier,
                       outcome_type_available=c("survival"),
-                      data_element="kaplan_meier_data",
+                      data_element="risk_stratification_data",
                       debug=debug_flag)
 
 # Test alignment of different plots, with missing data.
 familiar:::test_plot_ordering(plot_function=familiar:::plot_kaplan_meier,
-                              data_element="kaplan_meier_data",
+                              data_element="risk_stratification_data",
                               outcome_type_available=c("survival"),
                               debug=debug_flag)
 
 # Test alignment of different plots, with missing data.
 familiar:::test_plot_ordering(plot_function=familiar:::plot_kaplan_meier,
-                              data_element="kaplan_meier_data",
+                              data_element="risk_stratification_data",
                               outcome_type_available=c("survival"),
                               plot_args=list("facet_by"=c("learner", "fs_method", "data_set"),
                                              "color_by"="risk_group"),
