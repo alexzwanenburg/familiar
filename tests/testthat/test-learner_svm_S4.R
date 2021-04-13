6425f5a4c40fd7b817de482c30da7ef2d8303f2f
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2021-04-13 21:00:16 +0200

    Unit test for hyperparameter optimisation of svm learners.

--- tests/testthat/test-learner_svm_S4.R
+++ tests/testthat/test-learner_svm_S4.R
@@ -6,7 +6,17 @@ familiar:::test_all_learners_available(learners=familiar:::.get_available_svm_ep
 # Don't perform any further tests on CRAN due to time of running the complete test.
 testthat::skip_on_cran()
 
-# testthat::skip("Skipping svm tests. Some tests appear to get stuck when run with devtools::test as part of a complete test. Initialisation issues?")
+familiar:::test_hyperparameter_optimisation(learners=familiar:::.get_available_svm_c_learners(show_general=TRUE),
+                                            debug=FALSE,
+                                            parallel=FALSE)
+
+familiar:::test_hyperparameter_optimisation(learners=familiar:::.get_available_svm_nu_learners(show_general=TRUE),
+                                            debug=FALSE,
+                                            parallel=FALSE)
+
+familiar:::test_hyperparameter_optimisation(learners=familiar:::.get_available_svm_eps_learners(show_general=TRUE),
+                                            debug=FALSE,
+                                            parallel=FALSE)
 
 familiar:::test_all_learners_train_predict_vimp(learners=familiar:::.get_available_svm_c_learners(show_general=FALSE),
                                                 hyperparameter_list=list("binomial"=list("c"=-1.0,
