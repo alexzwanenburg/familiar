e25b8b8f5a00953abbe35b73d0b49c0ac4c66b60
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2021-04-14 11:39:56 +0200

    Implemented hyperparameter optimisation unit test for glmnet VIMP.

--- tests/testthat/test-vimp_glmnet_S4.R
+++ tests/testthat/test-vimp_glmnet_S4.R
@@ -3,8 +3,21 @@ familiar:::test_all_vimp_methods_available(vimp_methods=familiar:::.get_availabl
 familiar:::test_all_vimp_methods_available(vimp_methods=familiar:::.get_available_glmnet_lasso_vimp_methods(show_general=TRUE))
 familiar:::test_all_vimp_methods_available(vimp_methods=familiar:::.get_available_glmnet_elastic_net_vimp_methods(show_general=TRUE))
 
+familiar:::test_hyperparameter_optimisation(vimp_methods=familiar:::.get_available_glmnet_ridge_vimp_methods(show_general=FALSE),
+                                            debug=TRUE,
+                                            parallel=FALSE,
+                                            always_available=TRUE)
 familiar:::test_all_vimp_methods(vimp_methods=familiar:::.get_available_glmnet_ridge_vimp_methods(show_general=FALSE))
+
+familiar:::test_hyperparameter_optimisation(vimp_methods=familiar:::.get_available_glmnet_lasso_vimp_methods(show_general=FALSE),
+                                            debug=TRUE,
+                                            parallel=FALSE,
+                                            always_available=TRUE)
 familiar:::test_all_vimp_methods(vimp_methods=familiar:::.get_available_glmnet_lasso_vimp_methods(show_general=FALSE))
+
+familiar:::test_hyperparameter_optimisation(vimp_methods=familiar:::.get_available_glmnet_elastic_net_vimp_methods(show_general=TRUE),
+                                            debug=TRUE,
+                                            parallel=FALSE)
 familiar:::test_all_vimp_methods(vimp_methods=familiar:::.get_available_glmnet_elastic_net_vimp_methods(show_general=FALSE),
                                                 hyperparameter_list=list("count"=list("alpha"=0.50),
                                                                          "continuous"=list("alpha"=0.50),
