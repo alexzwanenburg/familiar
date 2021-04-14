57bc7b462611c223026045f4abd2fe7f83eed1c1
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2021-04-14 16:41:48 +0200

    Skip most of glmnet VIMP tests on CRAN due to time constraints.

--- tests/testthat/test-vimp_glmnet_S4.R
+++ tests/testthat/test-vimp_glmnet_S4.R
@@ -3,6 +3,9 @@ familiar:::test_all_vimp_methods_available(vimp_methods=familiar:::.get_availabl
 familiar:::test_all_vimp_methods_available(vimp_methods=familiar:::.get_available_glmnet_lasso_vimp_methods(show_general=TRUE))
 familiar:::test_all_vimp_methods_available(vimp_methods=familiar:::.get_available_glmnet_elastic_net_vimp_methods(show_general=TRUE))
 
+# Don't perform any further tests on CRAN due to time of running the complete test.
+testthat::skip_on_cran()
+
 familiar:::test_hyperparameter_optimisation(vimp_methods=familiar:::.get_available_glmnet_ridge_vimp_methods(show_general=FALSE),
                                             debug=TRUE,
                                             parallel=FALSE,
