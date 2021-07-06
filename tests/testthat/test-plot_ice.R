e5d5b1c82c65ca12db6adac58cc22c5c2b4e1daa
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2021-07-06 21:18:44 +0200

    Added second unit test to test anchor values.

--- tests/testthat/test-plot_ice.R
+++ tests/testthat/test-plot_ice.R
@@ -4,3 +4,13 @@ familiar:::test_plot_ordering(plot_function=familiar:::plot_ice,
                               data_element="ice_data",
                               outcome_type_available=c("survival"),
                               debug=TRUE)
+
+familiar:::test_plot_ordering(plot_function=familiar:::plot_ice,
+                              data_element="ice_data",
+                              outcome_type_available="survival",
+                              features=c("rx", "nodes"),
+                              plot_args=list("anchor_values"=list("rx"="Obs",
+                                                                  "nodes"=2.5)),
+                              sample_limit=20L,
+                              n_sample_points=10L,
+                              debug=TRUE)
