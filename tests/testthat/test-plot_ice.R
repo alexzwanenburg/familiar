7fb1ce4b75da45a8666b51686a1a6c40ecd98232
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2021-08-02 14:20:01 +0200

    Added some unit test.

--- tests/testthat/test-plot_ice.R
+++ tests/testthat/test-plot_ice.R
@@ -19,6 +19,33 @@ familiar:::test_plot_ordering(plot_function=familiar:::plot_ice,
                               debug=TRUE)
 
 
+# 1D plot with novelty.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_ice,
+                              data_element="ice_data",
+                              outcome_type_available="survival",
+                              features=c("nodes"),
+                              plot_args=list("anchor_values"=list("nodes"=2.5),
+                                             "value_scales"="figure"),
+                              sample_limit=20L,
+                              n_sample_points=10L,
+                              create_novelty_detector=TRUE,
+                              debug=TRUE)
+
+# 1D plot with novelty and colouring.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_ice,
+                              data_element="ice_data",
+                              outcome_type_available="multinomial",
+                              features=c("Sepal_Length"),
+                              plot_args=list("anchor_values"=list("Sepal_Length"=5.05),
+                                             "value_scales"="figure",
+                                             "split_by"=c("fs_method", "learner"),
+                                             "color_by"="positive_class",
+                                             "facet_by"="data_set"),
+                              sample_limit=20L,
+                              n_sample_points=10L,
+                              create_novelty_detector=TRUE,
+                              debug=TRUE)
+
 
 # 2D plot without novelty.
 familiar:::test_plot_ordering(plot_function=familiar:::plot_ice,
@@ -31,6 +58,8 @@ familiar:::test_plot_ordering(plot_function=familiar:::plot_ice,
                               sample_limit=20L,
                               n_sample_points=10L,
                               debug=TRUE)
+
+
 
 # 2D plot with novelty.
 familiar:::test_plot_ordering(plot_function=familiar:::plot_ice,
