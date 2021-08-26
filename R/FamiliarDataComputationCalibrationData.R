16f455aaf9054b02cc67cd0887f17f98b774d611
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2021-08-26 16:19:31 +0200

    Filter missing outcomes prior to creating data for calibration plots.

--- R/FamiliarDataComputationCalibrationData.R
+++ R/FamiliarDataComputationCalibrationData.R
@@ -225,9 +225,19 @@ setMethod("extract_calibration_data", signature(object="familiarEnsemble"),
                                 is_pre_processed=is_pre_processed)
   }
   
-  
   # Check if any predictions are valid.
   if(!any_predictions_valid(prediction_data, outcome_type=object@outcome_type)) return(NULL)
+  
+  # Remove data with missing predictions.
+  prediction_data <- remove_nonvalid_predictions(prediction_data,
+                                                 outcome_type=object@outcome_type)
+  
+  # Remove data with missing outcomes.
+  prediction_data <- remove_missing_outcomes(prediction_data,
+                                             outcome_type=object@outcome_type)
+  
+  # Check that any prediction data remain.
+  if(is_empty(prediction_data)) return(NULL)
   
   # Add positive class as an identifier.
   if(object@outcome_type %in% c("binomial")){
