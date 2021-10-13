ac91c22d61173486b08b9d17010c16c248d9c164
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2021-10-13 13:55:37 +0200

    Decision curves now don't have their confidence intervals cut off due to y-range.

--- R/PlotDecisionCurves.R
+++ R/PlotDecisionCurves.R
@@ -288,6 +288,13 @@ setMethod("plot_decision_curve", signature(object="familiarCollection"),
               y_range <- c(head(y_breaks, n=1),
                            tail(y_breaks, n=1))
             }
+            
+            # Adapt the confidence intervals so that they fit in the y-range.
+            if(conf_int_style != "none"){
+              x@data[curve_type == "model" & is.finite(ci_low) & ci_low < y_range[1] & ci_up > y_range[1], "ci_low":=y_range[1]]
+              x@data[curve_type == "model" & is.finite(ci_up) & ci_up > y_range[2] & ci_low < y_range[2], "ci_up":=y_range[2]]
+            }
+            
 
             if(object@outcome_type %in% c("binomial", "multinomial")){
               split_variable <- "positive_class"
