59a2a3558a648594d784283c80d76a82a6055fc0
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2021-11-11 17:24:31 +0100

    Division of sample clustering unit tests for faster asynchronous testing.

---
+++ tests/testthat/test-plot_sample_clustering_a.R
@@ -0,0 +1,49 @@
+# Don't perform any further tests on CRAN due to time of running the complete test.
+testthat::skip_on_cran()
+
+debug_flag <- FALSE
+
+# Generic test
+familiar:::test_plots(plot_function=familiar:::plot_sample_clustering,
+                      except_failed_survival_prediction=FALSE,
+                      outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
+                      data_element="feature_expressions",
+                      plot_args=list("verbose"=FALSE),
+                      debug=debug_flag)
+
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_sample_clustering,
+                              data_element="feature_expressions",
+                              outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
+                              plot_args=list("verbose"=FALSE),
+                              debug=debug_flag)
+
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_sample_clustering,
+                              data_element="feature_expressions",
+                              outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
+                              plot_args=list("facet_by"=c("learner", "fs_method", "data_set"),
+                                             "verbose"=FALSE),
+                              debug=debug_flag)
+
+# Try swapping the axis.
+familiar:::test_plot_ordering(plot_function=familiar:::plot_sample_clustering,
+                              data_element="feature_expressions",
+                              outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
+                              plot_args=list("facet_by"=c("learner", "fs_method", "data_set"),
+                                             "x_axis_by"="sample",
+                                             "y_axis_by"="feature",
+                                             "verbose"=FALSE),
+                              debug=debug_flag)
+
+# Try swapping the axis and putting the outcome below the heatmap
+familiar:::test_plot_ordering(plot_function=familiar:::plot_sample_clustering,
+                              data_element="feature_expressions",
+                              outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
+                              plot_args=list("facet_by"=c("learner", "fs_method", "data_set"),
+                                             "x_axis_by"="sample",
+                                             "y_axis_by"="feature",
+                                             "show_outcome"="bottom",
+                                             "verbose"=FALSE),
+                              debug=debug_flag)
+
--- tests/testthat/test-plot_sample_clustering_b.R
+++ tests/testthat/test-plot_sample_clustering_b.R
@@ -8,54 +8,9 @@ familiar:::test_plots(plot_function=familiar:::plot_sample_clustering,
                       except_failed_survival_prediction=FALSE,
                       outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
                       data_element="feature_expressions",
-                      plot_args=list("verbose"=FALSE),
-                      debug=debug_flag)
-
-# Generic test
-familiar:::test_plots(plot_function=familiar:::plot_sample_clustering,
-                      except_failed_survival_prediction=FALSE,
-                      outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
-                      data_element="feature_expressions",
                       plot_args=list("verbose"=FALSE,
                                      "show_normalised_data"="set_normalisation"),
                       debug=debug_flag)
-
-
-# Test alignment of different plots, with missing data.
-familiar:::test_plot_ordering(plot_function=familiar:::plot_sample_clustering,
-                              data_element="feature_expressions",
-                              outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
-                              plot_args=list("verbose"=FALSE),
-                              debug=debug_flag)
-
-# Test alignment of different plots, with missing data.
-familiar:::test_plot_ordering(plot_function=familiar:::plot_sample_clustering,
-                              data_element="feature_expressions",
-                              outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
-                              plot_args=list("facet_by"=c("learner", "fs_method", "data_set"),
-                                             "verbose"=FALSE),
-                              debug=debug_flag)
-
-# Try swapping the axis.
-familiar:::test_plot_ordering(plot_function=familiar:::plot_sample_clustering,
-                              data_element="feature_expressions",
-                              outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
-                              plot_args=list("facet_by"=c("learner", "fs_method", "data_set"),
-                                             "x_axis_by"="sample",
-                                             "y_axis_by"="feature",
-                                             "verbose"=FALSE),
-                              debug=debug_flag)
-
-# Try swapping the axis and putting the outcome below the heatmap
-familiar:::test_plot_ordering(plot_function=familiar:::plot_sample_clustering,
-                              data_element="feature_expressions",
-                              outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
-                              plot_args=list("facet_by"=c("learner", "fs_method", "data_set"),
-                                             "x_axis_by"="sample",
-                                             "y_axis_by"="feature",
-                                             "show_outcome"="bottom",
-                                             "verbose"=FALSE),
-                              debug=debug_flag)
 
 # No extra elements
 familiar:::test_plot_ordering(plot_function=familiar:::plot_sample_clustering,
