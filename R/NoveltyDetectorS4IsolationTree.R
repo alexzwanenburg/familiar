79e88f6be6da3ed75aae01bbfbbdba30ec945cec
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2022-01-14 12:24:01 +0100

    Completed get_default_hyperparameters, ..train and ..predict methods for familiarIsolationForest novelty detectors.

--- R/NoveltyDetectorS4IsolationTree.R
+++ R/NoveltyDetectorS4IsolationTree.R
@@ -41,142 +41,97 @@ setMethod("get_default_hyperparameters", signature(object="familiarIsolationFore
             
             # Initialise list and declare hyperparameter entries.
             param <- list()
-            param$sign_size   <- list()
             param$n_tree      <- list()
             param$sample_size <- list()
             param$m_try       <- list()
-            param$node_size   <- list()
             param$tree_depth  <- list()
-            param$split_rule  <- list()
-            param$alpha       <- list()
-            
-            # The following hyperparameters are only used for feature selection.
-            param$fs_forest_type <- list()
-            param$fs_vimp_method <- list()
-            
-            # If dt is not provided, return the list with hyperparameter /names only.
+            param$n_dim       <- list()
+
+            # If data is not provided, return the list with hyperparameter
+            # /names only.
             if(is.null(data)) return(param)
             
-            # Get the number of samples
-            n_samples <- data.table::uniqueN(data@data, by=get_id_columns(id_depth="series"))
+            # Get the number of instances. Note that we are not interested in
+            # samples per se, as we are assessing novelty.
+            n_samples <- nrow(data@data)
             
-            ###### Signature size ##############################################
-            param$sign_size <- .get_default_sign_size(data_obj=data)
+            # Get the number of features.
+            n_features <- get_n_features(x=data)
             
             
             ###### Number of trees #############################################
+            # We limit the number of trees to limit memory footprint.
+            default_n_trees <- log2(max(c(64, ceiling(sqrt(n_samples)))))
             
             # Note that the number of trees is defined in powers of 2, based on
             # Oshiro, T. M., Perez, P. S., & Baranauskas, J. A. (2012, July).
             # How many trees in a random forest?. In MLDM (pp. 154-168).
-            param$n_tree <- .set_hyperparameter(default=c(4, 8, 10), type="integer", range=c(4, 10),
-                                                valid_range=c(0, Inf), randomise=TRUE)
+            param$n_tree <- .set_hyperparameter(default=default_n_trees,
+                                                type="integer",
+                                                range=c(4, 10),
+                                                valid_range=c(0, Inf),
+                                                randomise=FALSE)
             
             
             ###### Sample size #################################################
+            # We limit the number of samples in each tree to limit memory
+            # footprint.
+            default_sample_size <- max(c(128, ceiling(sqrt(n_samples))))
+            if(n_samples < default_sample_size) default_sample_size <- n_samples
+            
+            # Express as fraction.
+            default_sample_size <- default_sample_size / n_samples
             
             # Note that the sample size is here noted as a fraction, which
             # corresponds to the usage in ranger.
-            param$sample_size <- .set_hyperparameter(default=c(0.30, 0.50, 0.70, 1.00), type="numeric", range=c(2/n_samples, 1.0),
-                                                     valid_range=c(0, 1), randomise=TRUE)
+            param$sample_size <- .set_hyperparameter(default=default_sample_size,
+                                                     type="numeric",
+                                                     range=c(2/n_samples, 1.0),
+                                                     valid_range=c(0, 1),
+                                                     randomise=FALSE)
             
             
             ##### Number of candidate features selected at node ################
             
             # Note that the number of features is here noted as a fraction, but
-            # is used in ranger as an integer. Familiar ensures that always at
-            # least 1 feature is available as a candidate.
-            param$m_try <- .set_hyperparameter(default=c(0.1, 0.3, 0.5, 1.0), type="numeric", range=c(0.0, 1.0),
-                                               randomise=TRUE)
-            
-            
-            ##### Terminal node size ###########################################
-            
-            # Number of instances in the terminal node. Larger terminal node
-            # sizes limit tree depth and overfitting.
-            
-            # Define the default range.
-            node_size_range <- c(5, floor(n_samples / 3))
-            
-            # Define the default values.
-            node_size_default <- c(5, 10, 20, 50)
-            node_size_default <- node_size_default[node_size_default >= node_size_range[1] &
-                                                     node_size_default <= node_size_range[2]]
-            
-            # Set the node_size parameter.
-            param$node_size <- .set_hyperparameter(default=node_size_default, type="integer", range=node_size_range,
-                                                   valid_range=c(1, Inf), randomise=TRUE)
-            
+            # is used in the isolation forest as an integer. Familiar ensures
+            # that always at least 1 feature is available as a candidate.
+            param$m_try <- .set_hyperparameter(default=3 / n_features,
+                                               type="numeric",
+                                               range=c(0.0, 1.0),
+                                               randomise=FALSE)
+
             
             ##### Maximum tree depth ###########################################
-            
+
             # Determines the depth trees are allowed to grow to. Larger depths
             # increase the risk of overfitting.
-            param$tree_depth <- .set_hyperparameter(default=c(1, 2, 3, 7), type="integer", range=c(1, 10),
-                                                    valid_range=c(1, Inf), randomise=TRUE)
-            
-            
-            ##### Splitting rule ###############################################
-            
-            # Availability of splitting rules is dependent on the type of
-            # outcome.
-            if(object@outcome_type %in% c("binomial", "multinomial")){
-              split_rule_range <- c("gini", "extratrees", "hellinger")
-              split_rule_default <- "gini"
-              
-            } else if(object@outcome_type %in% c("continuous", "count")){
-              split_rule_range <- c("variance", "extratrees", "maxstat", "beta")
-              split_rule_default <- "maxstat"
-              
-            } else if(object@outcome_type == "survival" ){
-              split_rule_range <- c("logrank", "extratrees", "C", "maxstat")
-              split_rule_default <- "maxstat"
-              
-            } else {
-              ..error_no_known_outcome_type(object@outcome_type)
-            }
-            
-            # Set the split_rule parameter.
-            param$split_rule <- .set_hyperparameter(default=split_rule_default, type="factor", range=split_rule_range,
+            param$tree_depth <- .set_hyperparameter(default=ceiling(log2(default_sample_size * n_samples)),
+                                                    type="integer",
+                                                    range=c(1, 10),
+                                                    valid_range=c(1, Inf),
                                                     randomise=FALSE)
             
             
-            ##### Significance threshold for splitting #########################
+            ##### Number of splitting dimensions ###############################
             
-            # Sets the significance level required to allow a split on a
-            # variable. It is only used with maxstat.
-            alpha_randomise <- split_rule_default == "maxstat"
-            
-            if(alpha_randomise){
-              default_values <- c(0.05, 0.1, 0.5, 1.0)
+            # Switch between extended and conventional isolation trees.
+            if(object@learner %in% c("isolation_forest", "extended_isolation_forest")){
+              default_n_dim <- 3
               
-            } else {
-              default_values <- c(0.05)
+            } else if(object@learner %in% c("simple_isolation_forest")){
+              default_n_dim <- 1
             }
             
-            # Set the alpha parameter
-            param$alpha <- .set_hyperparameter(default=default_values,
-                                               type="numeric",
-                                               range=c(10^-6, 1.0),
-                                               valid_range=c(0.0, 1.0),
-                                               randomise=alpha_randomise,
-                                               distribution="log")
+            # The number of splitting dimensions cannot be larger than the
+            # number of features.
+            if(default_n_dim > n_features) default_n_dim <- n_features
             
-            ##### Feature selection forest type ################################
-            
-            # Enables the construction of holdout forests. A conventional forest
-            # is grown by default.
-            param$fs_forest_type <- .set_hyperparameter(default="standard", type="factor",
-                                                        range=c("standard", "holdout"),
-                                                        randomise=FALSE)
-            
-            ##### Feature selection variable importance method #################
-            
-            # Enables the use of different variable importance methods. The
-            # permutation method is used by default.
-            param$fs_vimp_method <- .set_hyperparameter(default="permutation", type="factor",
-                                                        range=c("permutation", "impurity", "impurity_corrected"),
-                                                        randomise=FALSE)
+            param$n_dim <- .set_hyperparameter(default=default_n_dim,
+                                               type="integer",
+                                               range=c(1, 3),
+                                               valid_range=c(1, Inf),
+                                               randomise=FALSE)
             
             return(param)
           })
@@ -185,12 +140,9 @@ setMethod("get_default_hyperparameters", signature(object="familiarIsolationFore
 
 #####..train####
 setMethod("..train", signature(object="familiarIsolationForest", data="dataObject"),
-          function(object, data){
+          function(object, data, ...){
             browser()
-            # Aggregate repeated measurement data - ranger does not facilitate
-            # repeated measurements.
-            data <- aggregate_data(data=data)
-            
+
             # Check if the training data is ok.
             if(has_bad_training_data(object=object, data=data)) return(callNextMethod())
             
@@ -200,10 +152,34 @@ setMethod("..train", signature(object="familiarIsolationForest", data="dataObjec
             # Check that required packages are loaded and installed.
             require_package(object, "train")
             
-  
+            # Replace any ordered variables by factors. We do this because
+            # ordered features can not be handled using isotree.
+            ordered_features <- colnames(data@data)[sapply(data@data, is.ordered)]
+            for(current_feature in ordered_features){
+              data@data[[current_feature]] <- factor(x=data@data[[current_feature]],
+                                                     levels=levels(data@data[[current_feature]]),
+                                                     ordered=FALSE)
+            }
+            
+            # Extract hyperparameters from the object.
+            param <- object@hyperparameters
+            
+            # Create an isolation forest. Note that in addition to specifying
+            # the number of trees and the number of samples assessed for each
+            # tree, missing_action is set to "fail" -- this decreases model
+            # footprint and is not necessary as familiar has its own imputation
+            # routines.
+            detector <- isotree::isolation.forest(data=data@data[, mget(get_feature_columns(data))],
+                                                  sample_size=ceiling(param$sample_size * nrow(data@data)),
+                                                  ntrees=ceiling(2^(param$n_tree)),
+                                                  ndim=param$n_dim,
+                                                  ntry=max(c(1, ceiling(param$m_try * length(get_n_features(x=data))))),
+                                                  max_depth=param$tree_depth,
+                                                  nthreads=1L,
+                                                  missing_action="fail")
             
             # Add model
-            object@model <- model
+            object@model <- detector
             
             # Set learner version
             object <- set_package_version(object)
@@ -215,12 +191,38 @@ setMethod("..train", signature(object="familiarIsolationForest", data="dataObjec
 
 #####..predict#####
 setMethod("..predict", signature(object="familiarIsolationForest", data="dataObject"),
-          function(object, data, type="default", time=NULL, ...){
+          function(object, data, type="novelty", ...){
             
             # Check that required packages are loaded and installed.
             require_package(object, "predict")
             
-            browser()
+            # Check if the model was trained.
+            if(!model_is_trained(object)) return(callNextMethod())
+            
+            # Check if the data is empty.
+            if(is_empty(data)) return(callNextMethod())
+            
+            # Get a placeholder prediction table.
+            prediction_table <- get_placeholder_prediction_table(object=object,
+                                                                 data=data,
+                                                                 type="novelty")
+            
+            # Find and replace ordered features.
+            ordered_features <- colnames(data@data)[sapply(data@data, is.ordered)]
+            for(current_feature in ordered_features){
+              data@data[[current_feature]] <- factor(x=data@data[[current_feature]],
+                                                     levels=levels(data@data[[current_feature]]),
+                                                     ordered=FALSE)
+            }
+            
+            # Find novelty values.
+            novelty_values <- predict(object=object@novelty_detector,
+                                      newdata=data@data)
+            
+            # Store the novelty values in the table.
+            prediction_table[, "novelty":=novelty_values]
+            
+            return(prediction_table)
           })
 
 
