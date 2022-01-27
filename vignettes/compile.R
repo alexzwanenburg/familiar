a9c362d0e47bcf489fc390453e92bd4e26c38089
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2022-01-27 16:50:27 +0100

    Pre-compile vignettes.

--- vignettes/compile.R
+++ vignettes/compile.R
@@ -1,5 +1,12 @@
 # This file is used to compile vignettes prior to release.
 
+# Render RMD.orig-files to RMD. This is done for compute-heavy data.
+# See https://ropensci.org/blog/2019/12/08/precompute-vignettes/
+knitr::knit("vignettes/evaluation_and_explanation.Rmd.orig", output = "vignettes/evaluation_and_explanation.Rmd")
+
 # Renders vignettes to the docs_github folder in GitHub-flavoured markdown.
 rmarkdown::render('vignettes/introduction.Rmd', output_format="rmarkdown::github_document", output_file='../docs_github/introduction.md')
 rmarkdown::render('vignettes/feature_selection.Rmd', output_format="rmarkdown::github_document", output_file='../docs_github/feature_selection.md')
+rmarkdown::render('vignettes/learners.Rmd', output_format="rmarkdown::github_document", output_file='../docs_github/learners.md')
+rmarkdown::render('vignettes/performance_metrics.Rmd', output_format="rmarkdown::github_document", output_file='../docs_github/performance_metrics.md')
+rmarkdown::render('vignettes/evaluation_and_explanation.Rmd', output_format="rmarkdown::github_document", output_file='../docs_github/evaluation_and_explanation.md')
