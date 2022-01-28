94122ab96bcfaa7ab6d9738b218b26ed2a361496
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2022-01-28 16:22:08 +0100

    More hyperparameter optimisation to manual.

--- tests/testthat/test-learner_naive_bayes_S4.R
+++ tests/testthat/test-learner_naive_bayes_S4.R
@@ -4,10 +4,6 @@ familiar:::test_all_learners_available(learners=familiar:::.get_available_naive_
 # Don't perform any further tests on CRAN due to time of running the complete test.
 testthat::skip_on_cran()
 
-familiar:::test_hyperparameter_optimisation(learners=familiar:::.get_available_naive_bayes_learners(show_general=TRUE),
-                                            debug=FALSE,
-                                            parallel=FALSE)
-
 familiar:::test_all_learners_train_predict_vimp(learners=familiar:::.get_available_naive_bayes_learners(show_general=FALSE),
                                                 hyperparameter_list=list("binomial"=list("laplace"=0.0),
                                                                          "multinomial"=list("laplace"=0.0)),
@@ -118,3 +114,9 @@ testthat::test_that("Naive Bayes model can train on wide data", {
   # Valid predictions cannot be made.
   testthat::expect_equal(familiar:::any_predictions_valid(familiar:::.predict(wide_model, wide_data), outcome_type=wide_data@outcome_type), TRUE)
 })
+
+testthat::skip("Skip hyperparameter optimisation, unless manual.")
+
+familiar:::test_hyperparameter_optimisation(learners=familiar:::.get_available_naive_bayes_learners(show_general=TRUE),
+                                            debug=FALSE,
+                                            parallel=FALSE)
