0951aa1440fe95cef17614214e85f3f5693f9c3a
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2022-03-15 22:34:27 +0100

    Added generic methods to FamiliarHyperparameterLearner.R

--- R/FamiliarHyperparameterLearner.R
+++ R/FamiliarHyperparameterLearner.R
@@ -1,8 +1,162 @@
+#' @include FamiliarS4Generics.R
+#' @include FamiliarS4Classes.R
+
+
+##### promote_learner (hyperparameter learner) ---------------------------------
+setMethod("promote_learner", signature(object="familiarHyperparameterLearner"),
+          function(object){
+            
+            learner <- object@learner
+            
+            # if(learner %in% .get_available_naive_bayes_learners()){
+            #   # Naive bayes model
+            #   object <- methods::new("familiarNaiveBayes", object)
+            #   
+            # } else if(learner %in% .get_available_knn_learners()){
+            #   # k-nearest neighbours model with radial kernel
+            #   object <- methods::new("familiarKNN", object)
+            #   
+            # } else if(learner %in% .get_available_svm_c_learners()){
+            #   # C-classification support vector machines.
+            #   object <- methods::new("familiarSVMC", object)
+            #   
+            # } else if(learner %in% .get_available_svm_nu_learners()){
+            #   # Nu-classification and regression support vector machines.
+            #   object <- methods::new("familiarSVMNu", object)
+            #   
+            # } else if(learner %in% .get_available_svm_eps_learners()){
+            #   # Epsilon regression support vector machines.
+            #   object <- methods::new("familiarSVMEps", object)
+            #   
+            # } else if(learner %in% .get_available_glm_learners()){
+            #   # Generalised linear models
+            #   object <- methods::new("familiarGLM", object)
+            #   
+            # } else if(learner %in% .get_available_glmnet_ridge_learners()){
+            #   # Ridge penalised regression models
+            #   object <- methods::new("familiarGLMnetRidge", object)
+            #   
+            # } else if(learner %in% .get_available_glmnet_lasso_learners()){
+            #   # Lasso penalised regression models
+            #   object <- methods::new("familiarGLMnetLasso", object)
+            #   
+            # } else if(learner %in% .get_available_glmnet_elastic_net_learners()){
+            #   # Elastic net penalised regression models.
+            #   object <- methods::new("familiarGLMnetElasticNet", object)
+            #   
+            # } else if(learner %in% .get_available_mboost_lm_learners()){
+            #   # Boosted generalised linear models
+            #   object <- methods::new("familiarMBoostLM", object)
+            #   
+            # } else if(learner %in% .get_available_xgboost_lm_learners()){
+            #   # Extreme gradient boosted linear models
+            #   object <- methods::new("familiarXGBoostLM", object)
+            #   
+            # } else if(learner %in% .get_available_cox_learners()){
+            #   # Cox proportional hazards regression model
+            #   object <- methods::new("familiarCoxPH", object)
+            #   
+            # } else if(learner %in% .get_available_survival_regression_learners()){
+            #   # Fully parametric survival regression model
+            #   object <- methods::new("familiarSurvRegr", object)
+            #   
+            # } else if(learner %in% .get_available_rfsrc_learners()){
+            #   # Random forests for survival, regression, and classification
+            #   object <- methods::new("familiarRFSRC", object)
+            #   
+            # } else if(learner %in% .get_available_ranger_learners()){
+            #   # Ranger random forests
+            #   object <- methods::new("familiarRanger", object)
+            #   
+            # } else if(learner %in% .get_available_mboost_tree_learners()){
+            #   # Boosted regression trees
+            #   object <- methods::new("familiarMBoostTree", object)
+            #   
+            # } else if(learner %in% .get_available_xgboost_tree_learners()){
+            #   # Extreme gradient boosted trees
+            #   object <- methods::new("familiarXGBoostTree", object)
+            #   
+            # } else if(learner %in% .get_available_xgboost_dart_learners()){
+            #   # Extreme gradient boosted trees
+            #   object <- methods::new("familiarXGBoostDart", object)
+            #   
+            # } else if(learner %in% .get_available_glmnet_lasso_learners_test()){
+            #   # Lasso penalised regression models for testing purposes.
+            #   object <- methods::new("familiarGLMnetLassoTest", object)
+            # }
+            
+            # Returned object can be a standard familiarModel
+            return(object)
+          })
+
+
 ##### .train (hyperparameter learner) ------------------------------------------
+setMethod(".train", signature(object="familiarHyperparameterLearner", data="data.table"),
+          function(object,
+                   data,
+                   parameter_data=NULL,
+                   ...) {
+            # Train method for training hyperparameter models
+            
+            # Check if the class of object is a subclass of
+            # familiarHyperparameterLearner.
+            if(!is_subclass(class(object)[1], "familiarHyperparameterLearner")) object <- promote_learner(object)
+            
+            # Check whether data contains the optimisation_score column.
+            if(!"optimisation_score" %in% colnames(data)){
+              data <- .compute_hyperparameter_optimisation_score(score_table=data,
+                                                                 optimisation_function=object@optimisation_function)
+            } 
+            
+            # Merge data and parameter_data on param_id.
+            if(!is.null(parameter_data)){
+              data <- merge(x=data,
+                            y=parameter_data,
+                            all.x=TRUE,
+                            all.y=FALSE,
+                            by="param_id")
+            }
+            
+            # Get hyperparameter names from the target learner.
+            default_hyperparameters <- .get_learner_hyperparameters(learner=object@target_learner,
+                                                                    outcome_type=object@target_outcome_type,
+                                                                    names_only=TRUE)
+            
+            # Set hyperparameters in data.
+            target_hyperparameters <- intersect(colnames(data),default_hyperparameters)
+            if(length(target_hyperparameters) == 0) stop("The provided hyperparameter data does not contain any of the hyperparameters expected by the learner.")
+            object@target_hyperparameters <- target_hyperparameters
+            
+            if(has_bad_training_data(object=object, data=data)){
+              # Create a random search object in case training data are bad in
+              # some way or form.
+              object <- methods::new("familiarHyperparameterRandomSearch", object)
+            }
+            
+            # Train a new model based on data.
+            object <- ..train(object=object,
+                              data=data,
+                              ...)
+            
+            return(object)
+          })
 
 
 ##### .predict (hyperparameter learner) ----------------------------------------
-
+setMethod(".predict", signature(object="familiarHyperparameterLearner", data="data.table"),
+          function(object,
+                   data,
+                   type="default",
+                   percentile=NULL,
+                   ...) {
+            # Predict method for hyperparameter models. The type argument can be
+            # default (reports the model estimate), sd (reports the model
+            # estimate + standard deviation), or percentile, in which case the
+            # model estimate and the requested percentile are returned.
+            
+            
+            return(object)
+          })
 
 ##### show(hyperparameter learner) ---------------------------------------------
 
@@ -100,4 +254,19 @@ setMethod("..predict", signature(object="familiarHyperparameterLearner", data="d
             # Like ..train, this method is a capture-all for when predictions
             # fail.
             return(rep_len(-1.0, length.out=nrow(data)))
+          })
+
+
+#####has_bad_training_data######################################################
+setMethod("has_bad_training_data", signature(object="familiarHyperparameterLearner", data="data.table"),
+          function(object, data, ...){
+            
+            # One cannot train without data or on a single sample.
+            if(is_empty(data)) return(TRUE)
+            if(data.table::uniqueN(data@data, by=get_id_columns(id_depth="sample")) < 2) return(TRUE)
+            
+            # Check if all data are non-singular.
+            if(all(sapply(data[, mget(object@target_hyperparameters)], is_singular_data))) return(TRUE)
+            
+            return(FALSE)
           })
