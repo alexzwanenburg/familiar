ce2e804070ddd522fe51f2cb7fe8ff29ef03c72d
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2022-03-15 12:27:11 +0100

    New generic S4 class hyperparameter learners.

--- R/FamiliarS4Classes.R
+++ R/FamiliarS4Classes.R
@@ -1051,6 +1051,81 @@ setClass("familiarNoveltyDetector",
 
 
 
+#### familiarHyperparameterLearner ---------------------------------------------
+
+#' Hyperparameter learner.
+#'
+#' A familiarHyperparameterLearner object is a self-contained model that can be
+#' applied to predict optimisation scores for a set of hyperparameters.
+#'
+#' @slot name Name of the familiarHyperparameterLearner object.
+#' @slot learner Algorithm used to create the hyperparameter learner.
+#' @slot target_learner Algorithm for which the hyperparameters are being
+#'   learned.
+#' @slot optimisation_metric One or metrics used to generate the optimisation
+#'   score.
+#' @slot optimisation_function Function used to generate the optimisation score.
+#' @slot model The actual model trained using the specific algorithm, e.g. a
+#'   isolation forest from the `isotree` package.
+#' @slot target_hyperparameters The names of the hyperparameters that are used
+#'   to train the hyperparameter learner.
+#' @slot project_id Identifier of the project that generated the
+#'   familiarHyperparameterLearner object.
+#' @slot familiar_version Version of the familiar package.
+#' @slot package Name of package(s) required to executed the hyperparameter
+#'   learner itself, e.g. `laGP`.
+#' @slot package_version Version of the packages mentioned in the `package`
+#'   attribute.
+#'
+#' @details Hyperparameter learners are used to infer the optimisation score for
+#'   sets of hyperparameters. These are then used to either infer utility using
+#'   acquisition functions or to generate summary scores to identify the optimal
+#'   model.
+#'
+#' @export
+
+setClass("familiarHyperparameterLearner",
+         slots = list(
+           # Model name.
+           name = "character",
+           # Hyperparameter learner
+           learner = "character",
+           # Learner for which the hyperparameters are being learned.
+           target_learner = "character",
+           # Metric(s) used to generate the input data for optimisation score
+           # that is being learned.
+           optimisation_metric = "character",
+           # Function used to generate the optimisation score.
+           optimisation_function = "character",
+           # Model container
+           model = "ANY",
+           # Names of the hyperparameters that are being learned.
+           target_hyperparameters = "ANY",
+           # Project identifier for consistency tracking
+           project_id = "ANY",
+           # Package version for backward compatibility.
+           familiar_version = "ANY",
+           # Name of the package required to train the learner.
+           package = "ANY",
+           # Version of the learner for reproducibility.
+           package_version = "ANY"
+         ),
+         prototype = list(
+           name = character(0),
+           learner = NA_character_,
+           target_learner = NA_character_,
+           optimisation_metric = NA_character_,
+           optimisation_function = NA_character_,
+           model = NULL,
+           target_hyperparameters = NULL,
+           project_id = NULL,
+           familiar_version = NULL,
+           package = NULL,
+           package_version = NULL
+         )
+)
+
+
 
 #### familiarMetric ------------------------------------------------------------
 
