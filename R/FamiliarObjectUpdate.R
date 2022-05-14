fec6effbeb281b038f1b1e8082eaf9e8e0e9f92d
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2022-05-14 22:28:03 +0200

    Update of feature information parameters is now conditioned on them being present.

--- R/FamiliarObjectUpdate.R
+++ R/FamiliarObjectUpdate.R
@@ -270,64 +270,79 @@ setMethod("update_object", signature(object="featureInfo"),
             
             if(object@familiar_version < "1.2.0"){
               # Upgrade transformation parameters to a proper S4 object.
-              object@transformation_parameters <- ..create_transformation_parameter_skeleton(feature_name=object@name,
+              if(!is.null(object@transformation_parameters)){
+                object@transformation_parameters <- ..create_transformation_parameter_skeleton(feature_name=object@name,
+                                                                                               feature_type=object@feature_type,
+                                                                                               available=is_available(object),
+                                                                                               method=object@transformation_parameters$transform_method,
+                                                                                               lambda=object@transformation_parameters$transform_lambda)
+              }
+
+              # Upgrade normalisation parameters to a proper S4 object.
+              if(!is.null(object@normalisation_parameters)){
+                object@normalisation_parameters <- ..create_normalisation_parameter_skeleton(feature_name=object@name,
                                                                                              feature_type=object@feature_type,
                                                                                              available=is_available(object),
-                                                                                             method=object@transformation_parameters$transform_method,
-                                                                                             lambda=object@transformation_parameters$transform_lambda)
-              
-              # Upgrade normalisation parameters to a proper S4 object.
-              object@normalisation_parameters <- ..create_normalisation_parameter_skeleton(feature_name=object@name,
-                                                                                           feature_type=object@feature_type,
-                                                                                           available=is_available(object),
-                                                                                           method=object@normalisation_parameters$norm_method,
-                                                                                           shift=object@normalisation_parameters$norm_shift,
-                                                                                           scale=object@normalisation_parameters$norm_scale)
+                                                                                             method=object@normalisation_parameters$norm_method,
+                                                                                             shift=object@normalisation_parameters$norm_shift,
+                                                                                             scale=object@normalisation_parameters$norm_scale)
+              }
               
               # Upgrade batch normalisation parameters to a proper S4 object.
-              # Determine the method used for batch normalisation.
-              batch_normalisation_method <- unique(sapply(object@batch_normalisation_parameters, function(x) (x$norm_method)))
-              batch_normalisation_method <- setdiff(batch_normalisation_method, c("none", "unknown"))
-              if(is_empty(batch_normalisation_method)) batch_normalisation_method <- "none"
-              browser()
-              # Add container.
-              batch_normalisation_parameters <- ..create_batch_normalisation_parameter_skeleton(feature_name=object@name,
-                                                                                                feature_type=object@feature_type,
-                                                                                                available=is_available(object),
-                                                                                                method=batch_normalisation_method)
+              if(!is.null(object@batch_normalisation_parameters)){
+                # Determine the method used for batch normalisation.
+                batch_normalisation_method <- unique(sapply(object@batch_normalisation_parameters, function(x) (x$norm_method)))
+                batch_normalisation_method <- setdiff(batch_normalisation_method, c("none", "unknown"))
+                if(is_empty(batch_normalisation_method)) batch_normalisation_method <- "none"
+                
+                # Add container.
+                batch_normalisation_parameters <- ..create_batch_normalisation_parameter_skeleton(feature_name=object@name,
+                                                                                                  feature_type=object@feature_type,
+                                                                                                  available=is_available(object),
+                                                                                                  method=batch_normalisation_method)
+                
+                # Update the container contents.
+                batch_normalisation_parameters@batch_parameters <- mapply(FUN=function(x, batch, object){
+                  return(..create_normalisation_parameter_skeleton(feature_name=object@name,
+                                                                   feature_type=object@feature_type,
+                                                                   available=is_available(object),
+                                                                   method=x$norm_method,
+                                                                   batch=batch,
+                                                                   shift=x$norm_shift,
+                                                                   scale=x$norm_scale))
+                },
+                x=object@batch_normalisation_parameters,
+                batch=names(object@batch_normalisation_parameters),
+                MoreArgs=list("object"=object))
+                
+                # Update names.
+                names(batch_normalisation_parameters@batch_parameters) <- names(object@batch_normalisation_parameters)
+                
+                # Update batch normalisations parameters in the object.
+                object@batch_normalisation_parameters <- batch_normalisation_parameters
+              }
               
-              # Update the container contents.
-              batch_normalisation_parameters@batch_parameters <- mapply(FUN=function(x, batch, object){
-                return(..create_normalisation_parameter_skeleton(feature_name=object@name,
-                                                                 feature_type=object@feature_type,
-                                                                 available=is_available(object),
-                                                                 method=x$norm_method,
-                                                                 batch=batch,
-                                                                 shift=x$norm_shift,
-                                                                 scale=x$norm_scale))
-              },
-              x=object@batch_normalisation_parameters,
-              batch=names(object@batch_normalisation_parameters),
-              MoreArgs=list("object"=object))
+              # Update imputation parameters to a proper S4 object.
+              if(!is.null(object@imputation_parameters)){
+                
+                # Create an S4 skeleton.
+                imputation_object <- ..create_imputation_parameter_skeleton(feature_name=object@feature_name,
+                                                                            feature_type=object@feature_type,
+                                                                            available=is_available(object),
+                                                                            method="simple")
+                
+                # Attach to imputation object.
+                imputation_object@model <- object@imputation_parameters$common_value
+                imputation_object@required_features <- object@feature_name
+                
+                # Attach to object.
+                object@imputation_parameters <- imputation_object
+              }
               
-              # Update names.
-              names(batch_normalisation_parameters@batch_parameters) <- names(object@batch_normalisation_parameters)
-              
-              # Update batch normalisations parameters in the object.
-              object@batch_normalisation_parameters <- batch_normalisation_parameters
-              
-              # Upgrade imputation parameters to proper S4 objects. For now create an S4 skeleton.
-              imputation_object <- ..create_imputation_parameter_skeleton(feature_name=object@feature_name,
-                                                                          feature_type=object@feature_type,
-                                                                          available=is_available(object),
-                                                                          method="simple")
-              
-              # Attach to imputation object.
-              imputation_object@model <- object@imputation_parameters$common_value
-              imputation_object@required_features <- object@feature_name
-              
-              # Attach to object.
-              object@imputation_parameters <- imputation_object
+              # Update cluster parameters to a proper S4 object.
+              if(!is.null(object@cluster_parameters)){
+                
+              }
             }
             
             if(!methods::validObject(object)) stop("Could not update the featureInfo object to the most recent definition.")
