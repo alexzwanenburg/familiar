009b8a59b959afd5b88ca6672054d8e3cf3949af
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2022-11-03 16:13:27 +0100

    Added simple unit test for naive models.

---
+++ tests/testthat/test-naive_model.R
@@ -0,0 +1,14 @@
+testthat::skip_on_cran()
+
+debug_flag <- FALSE
+
+# Test that creates a single naive model.
+familiar:::integrated_test(
+  experimental_design="fs+mb",
+  fs_method="no_features",
+  cluster_method="none",
+  imputation_method="simple",
+  estimation_type="point",
+  parallel=FALSE,
+  debug=debug_flag
+)
