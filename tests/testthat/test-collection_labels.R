378ef444363a23776d9709536d7e4f971d5ae588
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2022-12-10 23:26:22 +0100

    WIP on unit test.

--- tests/testthat/test-collection_labels.R
+++ tests/testthat/test-collection_labels.R
@@ -3,7 +3,7 @@ outcome_type <- "survival"
 for(outcome_type in c("continuous", "multinomial",  "survival")){
   
   # Get data.
-  data <- familiar::test.create_good_data_set(outcome_type = outcome_type)
+  data <- familiar:::test.create_good_data_set(outcome_type = outcome_type)
   
   # Select simple regression models to train with.
   learner <- switch(
@@ -21,7 +21,7 @@ for(outcome_type in c("continuous", "multinomial",  "survival")){
     data=data,
     experimental_design="fs+mb",
     fs_method="mim",
-    imputation="simple",
+    imputation_method="simple",
     learner=learner,
     estimation_type="point",
     skip_evaluation_elements=skip_data_elements,
@@ -31,39 +31,92 @@ for(outcome_type in c("continuous", "multinomial",  "survival")){
   familiar_collection_list <- list(
     "collection"=experiment_data$familiarCollection,
     "model"=familiar::as_familiar_collection(
-      object=experiment_data$familiarModel[[1]],
+      object=experiment_data$familiarModel,
+      data=data,
+      familiar_data_names="development",
       estimation_type="point",
-      data_element=setdiff(familiar:::.get_available_data_elements()), skip_data_elements,
-      familiar_data_names="train")
+      data_element=setdiff(familiar:::.get_available_data_elements(), skip_data_elements))
   )
   
   #### class names -------------------------------------------------------------
   for(collection in familiar_collection_list){
     class_names <- familiar::get_class_names(collection)
+    
+    testthat::test_that(
+      "Class names are correct",
+      {
+        if(outcome_type %in% c("binomial", "multinomial")){
+          testthat::expect_setequal(class_names, levels(data@data$outcome))
+          
+        } else {
+          testthat::expect_equal(class_names, character(0L))
+        }
+      }
+    )
   }
   
   #### data set names ----------------------------------------------------------
   for(collection in familiar_collection_list){
     data_set_names <- familiar::get_data_set_names(collection)
+    
+    testthat::test_that(
+      "Dataset names are correct",
+      {
+        testthat::expect_setequal(data_set_names, "development")
+      }
+    )
   }
   
   #### feature names -----------------------------------------------------------
   for(collection in familiar_collection_list){
     feature_names <- familiar::get_feature_names(collection)
+    
+    testthat::test_that(
+      "Dataset names are correct",
+      {
+        testthat::expect_equal(all(feature_names %in% familiar:::get_feature_columns(data)), TRUE)
+      }
+    )
   }
   
   #### vimp names --------------------------------------------------------------
   for(collection in familiar_collection_list){
     vimp_names <- familiar::get_fs_method_names(collection)
+    
+    testthat::test_that(
+      "VIMP names are correct",
+      {
+        testthat::expect_equal(vimp_names, "mim")
+      }
+    )
   }
   
   #### learner names -----------------------------------------------------------
   for(collection in familiar_collection_list){
     learner_names <- familiar::get_learner_names(collection)
+    
+    testthat::test_that(
+      "Learner names are correct",
+      {
+        testthat::expect_equal(learner_names, learner)
+      }
+    )
   }
   
   #### risk group names --------------------------------------------------------
   for(collection in familiar_collection_list){
     risk_group_names <- familiar::get_risk_group_names(collection)
+    
+    testthat::test_that(
+      "Risk group names are correct",
+      {
+        if(outcome_type %in% c("survival")){
+          testthat::expect_setequal(risk_group_names, )
+          
+        } else {
+          testthat::expect_equal(risk_group_names, character(0L))
+        }
+      }
+    )
   }
 }
