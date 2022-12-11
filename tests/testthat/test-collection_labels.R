22c6bab7bb8ab8789a3b316c1ded6ab819d59ac5
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2022-12-11 18:42:23 +0100

    Update test-collection_labels.R

--- tests/testthat/test-collection_labels.R
+++ tests/testthat/test-collection_labels.R
@@ -60,20 +60,17 @@ for(outcome_type in c("continuous", "multinomial",  "survival")){
           # original labels.
           testthat::expect_equal(
             familiar::get_class_names(collection),
-            new_class_names
-          )
+            new_class_names)
           
           # Reorder levels.
           collection <- familiar::set_class_names(
             collection,
-            order=rev(new_class_names)
-          )
+            order=rev(new_class_names))
           
           # Expect that the labels are now re-ordered.
           testthat::expect_equal(
             familiar::get_class_names(collection),
-            rev(new_class_names)
-          )
+            rev(new_class_names))
           
         } else {
           testthat::expect_equal(class_names, character(0L))
@@ -90,6 +87,19 @@ for(outcome_type in c("continuous", "multinomial",  "survival")){
       "Dataset names are correct",
       {
         testthat::expect_setequal(data_set_names, "development")
+        
+        # Replace data set names.
+        new_data_set_names <- paste0("data_set", seq_along(data_set_names))
+        collection <- familiar::set_data_set_names(
+          collection,
+          old="development",
+          new=new_data_set_names)
+        
+        # Expect that the labels are the same and have the same order as the
+        # original labels.
+        testthat::expect_equal(
+          familiar::get_data_set_names(collection),
+          new_data_set_names)
       }
     )
   }
@@ -99,9 +109,35 @@ for(outcome_type in c("continuous", "multinomial",  "survival")){
     feature_names <- familiar::get_feature_names(collection)
     
     testthat::test_that(
-      "Dataset names are correct",
+      "Feature names are correct",
       {
         testthat::expect_equal(all(feature_names %in% familiar:::get_feature_columns(data)), TRUE)
+        
+        # Replace feature names.
+        new_feature_names <- paste0("feature_", seq_along(feature_names))
+        collection <- familiar::set_feature_names(
+          collection,
+          old=rev(feature_names),
+          new=rev(new_feature_names))
+        
+        # Expect that the labels are the same and have the same order as the
+        # original labels.
+        testthat::expect_equal(
+          familiar::get_feature_names(collection),
+          new_feature_names
+        )
+        
+        # Reorder levels.
+        collection <- familiar::set_feature_names(
+          collection,
+          order=rev(new_feature_names)
+        )
+        
+        # Expect that the labels are now re-ordered.
+        testthat::expect_equal(
+          familiar::get_feature_names(collection),
+          rev(new_feature_names)
+        )
       }
     )
   }
@@ -114,6 +150,19 @@ for(outcome_type in c("continuous", "multinomial",  "survival")){
       "VIMP names are correct",
       {
         testthat::expect_equal(vimp_names, "mim")
+        
+        # Replace vimp-method names.
+        new_vimp_names <- paste0("vimp_method_", seq_along(vimp_names))
+        collection <- familiar::set_fs_method_names(
+          collection,
+          old=vimp_names,
+          new=new_vimp_names)
+        
+        # Expect that the labels are the same and have the same order as the
+        # original labels.
+        testthat::expect_equal(
+          familiar::get_fs_method_names(collection),
+          new_vimp_names)
       }
     )
   }
@@ -126,6 +175,19 @@ for(outcome_type in c("continuous", "multinomial",  "survival")){
       "Learner names are correct",
       {
         testthat::expect_equal(learner_names, learner)
+        
+        # Replace learner names.
+        new_learner_names <- paste0("learner_", seq_along(learner_names))
+        collection <- familiar::set_learner_names(
+          collection,
+          old=learner_names,
+          new=new_learner_names)
+        
+        # Expect that the labels are the same and have the same order as the
+        # original labels.
+        testthat::expect_equal(
+          familiar::get_learner_names(collection),
+          new_learner_names)
       }
     )
   }
@@ -138,7 +200,30 @@ for(outcome_type in c("continuous", "multinomial",  "survival")){
       "Risk group names are correct",
       {
         if(outcome_type %in% c("survival")){
-          testthat::expect_setequal(risk_group_names, )
+          testthat::expect_setequal(risk_group_names, c("low", "moderate", "high"))
+          
+          # Replace risk-group names.
+          new_risk_group_names <- paste0("risk_group", seq_along(risk_group_names))
+          collection <- familiar::set_risk_group_names(
+            collection,
+            old=rev(risk_group_names),
+            new=rev(new_risk_group_names))
+          
+          # Expect that the labels are the same and have the same order as the
+          # original labels.
+          testthat::expect_equal(
+            familiar::get_risk_group_names(collection),
+            new_risk_group_names)
+          
+          # Reorder levels.
+          collection <- familiar::set_risk_group_names(
+            collection,
+            order=rev(new_risk_group_names))
+          
+          # Expect that the labels are now re-ordered.
+          testthat::expect_equal(
+            familiar::get_risk_group_names(collection),
+            rev(new_risk_group_names))
           
         } else {
           testthat::expect_equal(risk_group_names, character(0L))
