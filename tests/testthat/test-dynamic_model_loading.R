1be3c6ff46c2163a0961898f9e7486a054329b44
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-03-20 12:31:17 +0100

    Fixed code style issues.

--- tests/testthat/test-dynamic_model_loading.R
+++ tests/testthat/test-dynamic_model_loading.R
@@ -1,10 +1,11 @@
 testthat::skip_on_cran()
 
 # Perform an integrated test. Note that learner is set automatically.
-familiar:::integrated_test(dynamic_model_loading=TRUE,
-                           experimental_design="fs+mb",
-                           fs_method="none",
-                           cluster_method="none",
-                           imputation_method="simple",
-                           parallel=FALSE,
-                           estimation_type="point")
+familiar:::integrated_test(
+  dynamic_model_loading = TRUE,
+  experimental_design = "fs+mb",
+  fs_method = "none",
+  cluster_method = "none",
+  imputation_method = "simple",
+  parallel = FALSE,
+  estimation_type = "point")
