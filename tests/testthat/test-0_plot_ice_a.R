30f0e82b630bd8e8e9570555b216ed71cf52bf3e
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-03-20 09:50:57 +0100

    Fixed code style issues.

--- tests/testthat/test-0_plot_ice_a.R
+++ tests/testthat/test-0_plot_ice_a.R
@@ -1,11 +1,12 @@
-# Don't perform any further tests on CRAN due to time of running the complete test.
+# Don't perform any further tests on CRAN due to time of running the complete
+# test.
 testthat::skip_on_cran()
 
 debug_flag <- FALSE
 
-familiar:::test_plots(plot_function=familiar:::plot_ice,
-                      not_available_some_predictions_fail=FALSE,
-                      data_element="ice_data",
-                      debug=debug_flag)
-
-
+familiar:::test_plots(
+  plot_function = familiar:::plot_ice,
+  not_available_some_predictions_fail = FALSE,
+  data_element = "ice_data",
+  debug = debug_flag
+)
