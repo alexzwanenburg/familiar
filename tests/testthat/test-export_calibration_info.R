5aac5e9d41e433ef1909c635482803a0b940a5b8
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-03-20 12:38:00 +0100

    Fixed code style issues.

--- tests/testthat/test-export_calibration_info.R
+++ tests/testthat/test-export_calibration_info.R
@@ -1,22 +1,26 @@
-# Don't perform any further tests on CRAN due to time of running the complete test.
+# Don't perform any further tests on CRAN due to time of running the complete
+# test.
 testthat::skip_on_cran()
 
 debug_flag <- FALSE
 
-familiar:::test_export(export_function=familiar:::export_calibration_info,
-                       data_element="calibration_info",
-                       not_available_no_samples=FALSE,
-                       not_available_all_predictions_fail=FALSE,
-                       not_available_some_predictions_fail=FALSE,
-                       outcome_type_available=c("survival"),
-                       debug=debug_flag)
+familiar:::test_export(
+  export_function = familiar:::export_calibration_info,
+  data_element = "calibration_info",
+  not_available_no_samples = FALSE,
+  not_available_all_predictions_fail = FALSE,
+  not_available_some_predictions_fail = FALSE,
+  outcome_type_available = c("survival"),
+  debug = debug_flag
+)
 
-
-familiar:::test_export(export_function=familiar:::export_calibration_info,
-                       data_element="calibration_info",
-                       not_available_no_samples=FALSE,
-                       not_available_all_predictions_fail=FALSE,
-                       not_available_some_predictions_fail=FALSE,
-                       outcome_type_available=c("survival"),
-                       export_args=list("aggregate_results"=FALSE),
-                       debug=debug_flag)
+familiar:::test_export(
+  export_function = familiar:::export_calibration_info,
+  data_element = "calibration_info",
+  not_available_no_samples = FALSE,
+  not_available_all_predictions_fail = FALSE,
+  not_available_some_predictions_fail = FALSE,
+  outcome_type_available = c("survival"),
+  export_args = list("aggregate_results" = FALSE),
+  debug = debug_flag
+)
