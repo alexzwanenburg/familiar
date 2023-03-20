9864ca697df42befd72475269ec9e9b497a23dc1
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-03-20 12:43:54 +0100

    Fixed code style issues.

--- tests/testthat/test-export_risk_stratification_info.R
+++ tests/testthat/test-export_risk_stratification_info.R
@@ -4,27 +4,33 @@ testthat::skip_on_cran()
 debug_flag <- FALSE
 
 # Default
-familiar:::test_export(export_function=familiar:::export_risk_stratification_info,
-                       not_available_some_predictions_fail=FALSE,
-                       not_available_no_samples=FALSE,
-                       data_element="risk_stratification_info",
-                       outcome_type_available=c("survival"),
-                       debug=debug_flag)
+familiar:::test_export(
+  export_function = familiar:::export_risk_stratification_info,
+  not_available_some_predictions_fail = FALSE,
+  not_available_no_samples = FALSE,
+  data_element = "risk_stratification_info",
+  outcome_type_available = c("survival"),
+  debug = debug_flag
+)
 
 # With ensemble detail level.
-familiar:::test_export(export_function=familiar:::export_risk_stratification_info,
-                       not_available_some_predictions_fail=FALSE,
-                       not_available_no_samples=FALSE,
-                       data_element="risk_stratification_info",
-                       detail_level="ensemble",
-                       outcome_type_available=c("survival"),
-                       debug=debug_flag)
+familiar:::test_export(
+  export_function = familiar:::export_risk_stratification_info,
+  not_available_some_predictions_fail = FALSE,
+  not_available_no_samples = FALSE,
+  data_element = "risk_stratification_info",
+  detail_level = "ensemble",
+  outcome_type_available = c("survival"),
+  debug = debug_flag
+)
 
 # Without aggregation
-familiar:::test_export(export_function=familiar:::export_risk_stratification_info,
-                       not_available_some_predictions_fail=FALSE,
-                       not_available_no_samples=FALSE,
-                       data_element="risk_stratification_info",
-                       outcome_type_available=c("survival"),
-                       export_args=list("aggregate_results"=FALSE),
-                       debug=debug_flag)
+familiar:::test_export(
+  export_function = familiar:::export_risk_stratification_info,
+  not_available_some_predictions_fail = FALSE,
+  not_available_no_samples = FALSE,
+  data_element = "risk_stratification_info",
+  outcome_type_available = c("survival"),
+  export_args = list("aggregate_results" = FALSE),
+  debug = debug_flag
+)
