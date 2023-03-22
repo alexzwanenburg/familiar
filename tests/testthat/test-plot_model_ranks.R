77a34f37f525dbb82f6bdc52fe510f8e37842088
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-03-22 15:55:35 +0100

    Fixed code style issues.

--- tests/testthat/test-plot_model_ranks.R
+++ tests/testthat/test-plot_model_ranks.R
@@ -4,48 +4,61 @@ testthat::skip_on_cran()
 debug_flag <- FALSE
 
 # Generic test.
-familiar:::test_plots(plot_function=familiar:::plot_model_signature_occurrence,
-                      not_available_no_samples = FALSE,
-                      not_available_all_predictions_fail = FALSE,
-                      not_available_some_predictions_fail = FALSE,
-                      outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
-                      data_element="model_vimp",
-                      debug=debug_flag)
+familiar:::test_plots(
+  plot_function = familiar:::plot_model_signature_occurrence,
+  not_available_no_samples = FALSE,
+  not_available_all_predictions_fail = FALSE,
+  not_available_some_predictions_fail = FALSE,
+  outcome_type_available = c("count", "continuous", "binomial", "multinomial", "survival"),
+  data_element = "model_vimp",
+  debug = debug_flag
+)
 
 # Test alignment of different plots, with missing data.
-familiar:::test_plot_ordering(plot_function=familiar:::plot_model_signature_occurrence,
-                              data_element="model_vimp",
-                              outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
-                              debug=debug_flag)
+familiar:::test_plot_ordering(
+  plot_function = familiar:::plot_model_signature_occurrence,
+  data_element = "model_vimp",
+  outcome_type_available = c("count", "continuous", "binomial", "multinomial", "survival"),
+  debug = debug_flag
+)
 
 # Test alignment of different plots, with missing data.
-familiar:::test_plot_ordering(plot_function=familiar:::plot_model_signature_occurrence,
-                              data_element="model_vimp",
-                              outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
-                              plot_args=list("facet_by"=c("learner"),
-                                             "color_by"=c("fs_method")),
-                              debug=debug_flag)
-
+familiar:::test_plot_ordering(
+  plot_function = familiar:::plot_model_signature_occurrence,
+  data_element = "model_vimp",
+  outcome_type_available = c("count", "continuous", "binomial", "multinomial", "survival"),
+  plot_args = list(
+    "facet_by" = c("learner"),
+    "color_by" = c("fs_method")),
+  debug = debug_flag
+)
 
 # Generic test.
-familiar:::test_plots(plot_function=familiar:::plot_model_signature_variable_importance,
-                      not_available_no_samples = FALSE,
-                      not_available_all_predictions_fail = FALSE,
-                      not_available_some_predictions_fail = FALSE,
-                      outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
-                      data_element="model_vimp",
-                      debug=debug_flag)
+familiar:::test_plots(
+  plot_function = familiar:::plot_model_signature_variable_importance,
+  not_available_no_samples = FALSE,
+  not_available_all_predictions_fail = FALSE,
+  not_available_some_predictions_fail = FALSE,
+  outcome_type_available = c("count", "continuous", "binomial", "multinomial", "survival"),
+  data_element = "model_vimp",
+  debug = debug_flag
+)
 
 # Test alignment of different plots, with missing data.
-familiar:::test_plot_ordering(plot_function=familiar:::plot_model_signature_variable_importance,
-                              data_element="model_vimp",
-                              outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
-                              debug=debug_flag)
+familiar:::test_plot_ordering(
+  plot_function = familiar:::plot_model_signature_variable_importance,
+  data_element = "model_vimp",
+  outcome_type_available = c("count", "continuous", "binomial", "multinomial", "survival"),
+  debug = debug_flag
+)
 
 # Test alignment of different plots, with missing data.
-familiar:::test_plot_ordering(plot_function=familiar:::plot_model_signature_variable_importance,
-                              data_element="model_vimp",
-                              outcome_type_available=c("count", "continuous", "binomial", "multinomial", "survival"),
-                              plot_args=list("facet_by"=c("learner"),
-                                             "color_by"=c("fs_method")),
-                              debug=debug_flag)
+familiar:::test_plot_ordering(
+  plot_function = familiar:::plot_model_signature_variable_importance,
+  data_element = "model_vimp",
+  outcome_type_available = c("count", "continuous", "binomial", "multinomial", "survival"),
+  plot_args = list(
+    "facet_by" = c("learner"),
+    "color_by" = c("fs_method")),
+  debug = debug_flag
+)
