8be56e0075f4bbce0805acd361d0c3116c8d2879
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-03-22 15:39:13 +0100

    Fixed code style issues.

--- tests/testthat/test-parallel_backend.R
+++ tests/testthat/test-parallel_backend.R
@@ -1,38 +1,44 @@
 testthat::skip_on_cran()
 
 # Perform an integrated test. Note that learner is set automatically.
-familiar:::integrated_test(backend_type="none",
-                           experimental_design="bt(fs+mb,20)",
-                           parallel_nr_cores=2,
-                           fs_method="none",
-                           cluster_method="none",
-                           imputation_method="simple",
-                           parallel=TRUE,
-                           skip_evaluation_elements="all")
+familiar:::integrated_test(
+  backend_type = "none",
+  experimental_design = "bt(fs+mb,20)",
+  parallel_nr_cores = 2,
+  fs_method = "none",
+  cluster_method = "none",
+  imputation_method = "simple",
+  parallel = TRUE,
+  skip_evaluation_elements = "all"
+)
 
 # Perform an integrated test using the socket backend.
-familiar:::integrated_test(backend_type="socket_server",
-                           experimental_design="bt(fs+mb,20)",
-                           parallel_nr_cores=2,
-                           fs_method="none",
-                           cluster_method="none",
-                           imputation_method="simple",
-                           parallel=TRUE,
-                           skip_evaluation_elements="all")
+familiar:::integrated_test(
+  backend_type = "socket_server",
+  experimental_design = "bt(fs+mb,20)",
+  parallel_nr_cores = 2,
+  fs_method = "none",
+  cluster_method = "none",
+  imputation_method = "simple",
+  parallel = TRUE,
+  skip_evaluation_elements = "all"
+)
 
-# Test external 
+# Test external
 
 # Start cluster
-cl <- familiar:::.start_cluster(n_cores=2L, cluster_type="psock")
+cl <- familiar:::.start_cluster(n_cores = 2L, cluster_type = "psock")
 
-familiar:::integrated_test(backend_type="none",
-                           cl=cl,
-                           experimental_design="bt(fs+mb,20)",
-                           fs_method="mrmr",
-                           cluster_method="none",
-                           imputation_method="simple",
-                           parallel=TRUE,
-                           skip_evaluation_elements="all",
-                           debug=TRUE)
+familiar:::integrated_test(
+  backend_type = "none",
+  cl = cl,
+  experimental_design = "bt(fs+mb,20)",
+  fs_method = "mrmr",
+  cluster_method = "none",
+  imputation_method = "simple",
+  parallel = TRUE,
+  skip_evaluation_elements = "all",
+  debug = TRUE
+)
 
-cl <- familiar:::.terminate_cluster(cl=cl)
+cl <- familiar:::.terminate_cluster(cl = cl)
