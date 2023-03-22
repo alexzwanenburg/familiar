cc9138d1dc28df14ee6c4dfe6db196b3d1d758dd
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-03-22 15:28:49 +0100

    Fixed code style issues

--- tests/testthat/test-novelty_none_S4.R
+++ tests/testthat/test-novelty_none_S4.R
@@ -1,12 +1,15 @@
 # Test if detectors are available.
-familiar:::test_all_novelty_detectors_available(detectors=familiar:::.get_available_none_detectors())
+familiar:::test_all_novelty_detectors_available(
+  detectors = familiar:::.get_available_none_detectors())
 
 # Don't perform any further tests on CRAN due to time of running the test.
 testthat::skip_on_cran()
 
-familiar:::test_all_novelty_detectors(detectors=familiar:::.get_available_none_detectors(),
-                                      except_train=familiar:::.get_available_none_detectors(),
-                                      can_trim=FALSE)
+familiar:::test_all_novelty_detectors(
+  detectors = familiar:::.get_available_none_detectors(),
+  except_train = familiar:::.get_available_none_detectors(),
+  can_trim = FALSE)
 
-familiar:::test_all_novelty_detectors_parallel(detectors=familiar:::.get_available_none_detectors(),
-                                               except_train=familiar:::.get_available_none_detectors())
+familiar:::test_all_novelty_detectors_parallel(
+  detectors = familiar:::.get_available_none_detectors(),
+  except_train = familiar:::.get_available_none_detectors())
