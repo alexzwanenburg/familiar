129790da2157994aace1d1f7ea49fdc3cf2013f5
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-06-27 17:41:10 +0200

    Updated Cox PH unit tests.

--- tests/testthat/test-learner_cox_s4.R
+++ tests/testthat/test-learner_cox_s4.R
@@ -14,7 +14,6 @@ familiar:::test_all_learners_parallel_train_predict_vimp(
 
 # Create test data sets.
 good_data <- familiar:::test_create_good_data("survival")
-wide_data <- familiar:::test_create_wide_data("survival")
 
 # Train the model using the good dataset.
 good_model <- familiar:::test_train(
@@ -23,14 +22,6 @@ good_model <- familiar:::test_train(
   imputation_method = "simple",
   hyperparameter_list = list("sign_size" = familiar:::get_n_features(good_data)),
   learner = "cox")
-
-# Train the model using wide data.
-wide_model <- suppressWarnings(familiar:::test_train(
-  data = wide_data,
-  cluster_method = "none",
-  imputation_method = "simple",
-  hyperparameter_list = list("sign_size" = familiar:::get_n_features(wide_data)),
-  learner = "cox"))
 
 
 testthat::test_that("Cox model trained correctly", {
@@ -58,52 +49,16 @@ testthat::test_that("Cox model trained correctly", {
 testthat::test_that("Cox model has variable importance", {
   # Extract the variable importance table.
   vimp_table <- familiar:::get_vimp_table(good_model)
-
-  # Expect that the vimp table has two rows.
-  testthat::expect_equal(nrow(vimp_table), 3)
-
+  
+  # Expect that the vimp table has six rows.
+  testthat::expect_equal(nrow(vimp_table), 6L)
+  
   # Expect that the names are the same as that of the features.
-  testthat::expect_equal(
-    all(familiar:::get_feature_columns(good_data) %in% vimp_table$name),
-    TRUE)
-
-  # Expect that nodes has rank 1 and rx has rank 2.
-  testthat::expect_equal(vimp_table[rank == 1, ]$name, "nodes")
-  testthat::expect_equal(vimp_table[rank == 2, ]$name, "rx")
-})
-
-
-testthat::test_that("Cox model does not train for wide data", {
-  # Model trained
-  testthat::expect_equal(familiar:::model_is_trained(wide_model), FALSE)
-
-  # No variable importance table.
-  testthat::expect_equal(familiar:::is_empty(familiar:::get_vimp_table(wide_model)), TRUE)
-
-  # No valid predictions.
-  testthat::expect_equal(
-    familiar:::any_predictions_valid(
-      familiar:::.predict(wide_model, wide_data),
-      outcome_type = wide_data@outcome_type),
-    FALSE)
-
-  # No valid survival probability predictions.
-  testthat::expect_equal(
-    familiar:::any_predictions_valid(
-      familiar:::.predict(wide_model, wide_data, type = "survival_probability", time = 1000),
-      outcome_type = wide_data@outcome_type),
-    FALSE)
-
-  # Test that specific warnings and errors appear.
-  testthat::expect_equal(length(wide_model@messages$warning), 1L)
-  testthat::expect_equal(
-    grepl(x = wide_model@messages$warning, pattern = "did not converge", fixed = TRUE),
-    TRUE)
-
-  testthat::expect_equal(length(wide_model@messages$error), 1L)
-  testthat::expect_equal(
-    grepl(x = wide_model@messages$error, pattern = "did not converge", fixed = TRUE),
-    TRUE)
+  testthat::expect_true(
+    all(familiar:::get_feature_columns(good_data) %in% vimp_table$name))
+  
+  # Feature 1 is most important.
+  testthat::expect_equal(vimp_table[rank == 1, ]$name, "feature_1")
 })
 
 
