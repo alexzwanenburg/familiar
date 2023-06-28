843e52c820994fa6957ecdd39f81b57a2a668ce7
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-06-28 15:05:56 +0200

    Reworked XGboost tree learner unit tests.

--- tests/testthat/test-learner_xgboost_tree_S4.R
+++ tests/testthat/test-learner_xgboost_tree_S4.R
@@ -103,7 +103,6 @@ familiar:::test_all_learners_parallel_train_predict_vimp(
 
 # Create test data sets.
 good_data <- familiar:::test_create_good_data("continuous")
-wide_data <- familiar:::test_create_wide_data("continuous")
 
 # Train the model using the good dataset.
 good_model <- familiar:::test_train(
@@ -112,23 +111,6 @@ good_model <- familiar:::test_train(
   imputation_method = "simple",
   hyperparameter_list = list(
     "sign_size" = familiar:::get_n_features(good_data),
-    "n_boost" = 2,
-    "learning_rate" = -1,
-    "lambda" = 0.0,
-    "alpha" = -6.0,
-    "min_child_weight" = 1.04,
-    "tree_depth" = 3,
-    "sample_size" = 1.0,
-    "gamma" = -6.0),
-  learner = "xgboost_tree_gaussian")
-
-# Train the model using wide data.
-wide_model <- familiar:::test_train(
-  data = wide_data,
-  cluster_method = "none",
-  imputation_method = "simple",
-  hyperparameter_list = list(
-    "sign_size" = familiar:::get_n_features(wide_data),
     "n_boost" = 2,
     "learning_rate" = -1,
     "lambda" = 0.0,
@@ -153,42 +135,23 @@ testthat::test_that("Extreme gradient boosting tree model trained correctly", {
 testthat::test_that("Extreme gradient boosting tree model has variable importance", {
   # Extract the variable importance table.
   vimp_table <- familiar:::get_vimp_table(good_model)
-
-  # Expect that the vimp table has two rows.
-  testthat::expect_equal(nrow(vimp_table), 10)
-
+  
+  # Expect that the vimp table has six rows.
+  testthat::expect_equal(nrow(vimp_table), 6L)
+  
   # Expect that the names are the same as that of the features.
-  testthat::expect_true(all(vimp_table$name %in% familiar:::get_feature_columns(good_data)))
-
-  # Expect that avginc has rank 1 and calwpct has rank 2.
-  testthat::expect_equal(vimp_table[rank == 1, ]$name, "avginc")
-  testthat::expect_equal(vimp_table[rank == 2, ]$name, "calwpct")
+  testthat::expect_true(
+    all(familiar:::get_feature_columns(good_data) %in% vimp_table$name))
+  
+  # Feature 1 is most important.
+  testthat::expect_equal(vimp_table[rank == 1, ]$name, "feature_1")
 })
 
-testthat::test_that("Extreme gradient boosting tree model can train on wide data", {
-  # Model trained
-  testthat::expect_equal(familiar:::model_is_trained(wide_model), TRUE)
-
-  # Variable importance table is absent.
-  testthat::expect_true(familiar:::is_empty(familiar:::get_vimp_table(wide_model)))
-
-  # Valid predictions.
-  testthat::expect_true(familiar:::any_predictions_valid(
-    familiar:::.predict(wide_model, wide_data),
-    outcome_type = wide_data@outcome_type))
-
-  # Test that no deprecation warnings are given.
-  familiar:::test_not_deprecated(wide_model@messages$warning)
-
-  # Test that no errors appear.
-  testthat::expect_equal(wide_model@messages$error, NULL)
-})
 
 # Binomial tests----------------------------------------------------------------
 
 # Create test data sets.
 good_data <- familiar:::test_create_good_data("binomial")
-wide_data <- familiar:::test_create_wide_data("binomial")
 
 # Train the model using the good dataset.
 good_model <- familiar:::test_train(
@@ -197,23 +160,6 @@ good_model <- familiar:::test_train(
   imputation_method = "simple",
   hyperparameter_list = list(
     "sign_size" = familiar:::get_n_features(good_data),
-    "n_boost" = 2,
-    "learning_rate" = -1,
-    "lambda" = 0.0,
-    "alpha" = -6.0,
-    "min_child_weight" = 1.04,
-    "tree_depth" = 3,
-    "sample_size" = 1.0,
-    "gamma" = -6.0),
-  learner = "xgboost_tree_logistic")
-
-# Train the model using wide data.
-wide_model <- familiar:::test_train(
-  data = wide_data,
-  cluster_method = "none",
-  imputation_method = "simple",
-  hyperparameter_list = list(
-    "sign_size" = familiar:::get_n_features(wide_data),
     "n_boost" = 2,
     "learning_rate" = -1,
     "lambda" = 0.0,
@@ -235,47 +181,26 @@ testthat::test_that("Extreme gradient boosting tree model trained correctly", {
   testthat::expect_equal(good_model@messages$error, NULL)
 })
 
-
 testthat::test_that("Extreme gradient boosting tree model has variable importance", {
   # Extract the variable importance table.
   vimp_table <- familiar:::get_vimp_table(good_model)
-
-  # Expect that the vimp table has two rows.
-  testthat::expect_equal(nrow(vimp_table), 6)
-
+  
+  # Expect that the vimp table has up to six rows.
+  testthat::expect_lte(nrow(vimp_table), 6L)
+  
   # Expect that the names are the same as that of the features.
-  testthat::expect_true(all(vimp_table$name %in% familiar:::get_feature_columns(good_data)))
-
-  # Expect that cell_shape_uniformity has rank 1 and epithelial_cell_size has rank 2.
-  testthat::expect_equal(vimp_table[rank == 1, ]$name, "cell_shape_uniformity")
-  testthat::expect_equal(vimp_table[rank == 2, ]$name, "epithelial_cell_size")
+  testthat::expect_true(
+    all(vimp_table$name %in% familiar:::get_feature_columns(good_data)))
+  
+  # Feature 1 is most important.
+  testthat::expect_equal(vimp_table[rank == 1, ]$name, "feature_1")
 })
 
-
-testthat::test_that("Extreme gradient boosting tree model can train on wide data", {
-  # Model trained
-  testthat::expect_equal(familiar:::model_is_trained(wide_model), TRUE)
-
-  # Variable importance table is absent
-  testthat::expect_true(familiar:::is_empty(familiar:::get_vimp_table(wide_model)))
-
-  # Valid predictions.
-  testthat::expect_true(familiar:::any_predictions_valid(
-    familiar:::.predict(wide_model, wide_data),
-    outcome_type = wide_data@outcome_type))
-
-  # Test that no deprecation warnings are given.
-  familiar:::test_not_deprecated(wide_model@messages$warning)
-
-  # Test that no errors appear.
-  testthat::expect_equal(wide_model@messages$error, NULL)
-})
 
 # Multinomial tests-------------------------------------------------------------
 
 # Create test data sets.
 good_data <- familiar:::test_create_good_data("multinomial")
-wide_data <- familiar:::test_create_wide_data("multinomial")
 
 # Train the model using the good dataset.
 good_model <- familiar:::test_train(
@@ -284,23 +209,6 @@ good_model <- familiar:::test_train(
   imputation_method = "simple",
   hyperparameter_list = list(
     "sign_size" = familiar:::get_n_features(good_data),
-    "n_boost" = 2,
-    "learning_rate" = -1,
-    "lambda" = 0.0,
-    "alpha" = -6.0,
-    "min_child_weight" = 1.04,
-    "tree_depth" = 3,
-    "sample_size" = 1.0,
-    "gamma" = -6.0),
-  learner = "xgboost_tree_logistic")
-
-# Train the model using wide data.
-wide_model <- familiar:::test_train(
-  data = wide_data,
-  cluster_method = "none",
-  imputation_method = "simple",
-  hyperparameter_list = list(
-    "sign_size" = familiar:::get_n_features(wide_data),
     "n_boost" = 2,
     "learning_rate" = -1,
     "lambda" = 0.0,
@@ -322,46 +230,26 @@ testthat::test_that("Extreme gradient boosting tree model trained correctly", {
   testthat::expect_equal(good_model@messages$error, NULL)
 })
 
-
 testthat::test_that("Extreme gradient boosting tree model has variable importance", {
   # Extract the variable importance table.
   vimp_table <- familiar:::get_vimp_table(good_model)
-
-  # Expect that the vimp table has two rows.
-  testthat::expect_equal(nrow(vimp_table), 4)
-
+  
+  # Expect that the vimp table has six rows.
+  testthat::expect_equal(nrow(vimp_table), 6L)
+  
   # Expect that the names are the same as that of the features.
-  testthat::expect_true(all(familiar:::get_feature_columns(good_data) %in% vimp_table$name))
-
-  # Expect that cell_shape_uniformity has rank 1 and bare_nuclei has rank 2.
-  testthat::expect_equal(vimp_table[rank == 1, ]$name, "Petal_Length")
-  testthat::expect_equal(vimp_table[rank == 2, ]$name, "Petal_Width")
+  testthat::expect_true(
+    all(familiar:::get_feature_columns(good_data) %in% vimp_table$name))
+  
+  # Feature 1 is most important.
+  testthat::expect_equal(vimp_table[rank == 1, ]$name, "feature_1")
 })
 
-testthat::test_that("Extreme gradient boosting tree model can train on wide data", {
-  # Model trained
-  testthat::expect_equal(familiar:::model_is_trained(wide_model), TRUE)
-
-  # Variable importance table is absent.
-  testthat::expect_true(familiar:::is_empty(familiar:::get_vimp_table(wide_model)))
-
-  # Valid predictions.
-  testthat::expect_true(familiar:::any_predictions_valid(
-    familiar:::.predict(wide_model, wide_data),
-    outcome_type = wide_data@outcome_type))
-
-  # Test that no deprecation warnings are given.
-  familiar:::test_not_deprecated(wide_model@messages$warning)
-
-  # Test that no errors appear.
-  testthat::expect_equal(wide_model@messages$error, NULL)
-})
 
 # Survival tests----------------------------------------------------------------
 
 # Create test data sets.
 good_data <- familiar:::test_create_good_data("survival")
-wide_data <- familiar:::test_create_wide_data("survival")
 
 # Train the model using the good dataset.
 good_model <- familiar:::test_train(
@@ -370,24 +258,6 @@ good_model <- familiar:::test_train(
   imputation_method = "simple",
   hyperparameter_list = list(
     "sign_size" = familiar:::get_n_features(good_data),
-    "n_boost" = 2,
-    "learning_rate" = -1,
-    "lambda" = 0.0,
-    "alpha" = -6.0,
-    "min_child_weight" = 1.04,
-    "tree_depth" = 3,
-    "sample_size" = 1.0,
-    "gamma" = -6.0),
-  time_max = 1832,
-  learner = "xgboost_tree_cox")
-
-# Train the model using wide data.
-wide_model <- familiar:::test_train(
-  data = wide_data,
-  cluster_method = "none",
-  imputation_method = "simple",
-  hyperparameter_list = list(
-    "sign_size" = familiar:::get_n_features(wide_data),
     "n_boost" = 2,
     "learning_rate" = -1,
     "lambda" = 0.0,
@@ -423,42 +293,18 @@ testthat::test_that("Extreme gradient boosting tree model trained correctly", {
 testthat::test_that("Extreme gradient boosting tree model has variable importance", {
   # Extract the variable importance table.
   vimp_table <- familiar:::get_vimp_table(good_model)
-
-  # Expect that the vimp table has three rows.
-  testthat::expect_equal(nrow(vimp_table) %in% c(2, 3), TRUE)
-
+  
+  # Expect that the vimp table has six rows.
+  testthat::expect_equal(nrow(vimp_table), 6L)
+  
   # Expect that the names are the same as that of the features.
   testthat::expect_true(
-    any(familiar:::get_feature_columns(good_data) %in% vimp_table$name))
-
-  # Expect that nodes has rank 1 and rx has rank 2.
-  testthat::expect_equal(vimp_table[rank == 1, ]$name, "nodes")
-  testthat::expect_equal(vimp_table[rank == 2, ]$name, "rx")
+    all(familiar:::get_feature_columns(good_data) %in% vimp_table$name))
+  
+  # Feature 1 is most important.
+  testthat::expect_equal(vimp_table[rank == 1, ]$name, "feature_1")
 })
 
-testthat::test_that("Extreme gradient boosting tree model can train and predict on wide data", {
-  # Model was trained
-  testthat::expect_equal(familiar:::model_is_trained(wide_model), TRUE)
-
-  # Variable importance table is absent
-  testthat::expect_true(familiar:::is_empty(familiar:::get_vimp_table(wide_model)))
-
-  # Valid predictions are present.
-  testthat::expect_true(familiar:::any_predictions_valid(
-    familiar:::.predict(wide_model, wide_data),
-    outcome_type = wide_data@outcome_type))
-
-  # Valid survival probability predictions can be made.
-  testthat::expect_true(familiar:::any_predictions_valid(
-    familiar:::.predict(wide_model, wide_data, type = "survival_probability", time = 1000),
-    outcome_type = wide_data@outcome_type))
-
-  # Test that no deprecation warnings are given.
-  familiar:::test_not_deprecated(wide_model@messages$warning)
-
-  # Test that no errors appear.
-  testthat::expect_equal(wide_model@messages$error, NULL)
-})
 
 testthat::skip("Skip hyperparameter optimisation, unless manual.")
 
