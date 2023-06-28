8c2025e537f46ee6b1542ae8faa4b7ccc9ec01c1
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-06-28 09:13:05 +0200

    Reworked survival regression unit tests.

--- tests/testthat/test-learner_survival_regression_S4.R
+++ tests/testthat/test-learner_survival_regression_S4.R
@@ -12,9 +12,11 @@ familiar:::test_all_learners_train_predict_vimp(
 familiar:::test_all_learners_parallel_train_predict_vimp(
   learners = familiar:::.get_available_survival_regression_learners(show_general = FALSE))
 
+
+# Survival tests ---------------------------------------------------------------
+
 # Create test data sets.
 good_data <- familiar:::test_create_good_data("survival")
-wide_data <- familiar:::test_create_wide_data("survival")
 
 # Train the model using the good dataset.
 good_model <- familiar:::test_train(
@@ -23,14 +25,6 @@ good_model <- familiar:::test_train(
   imputation_method = "simple",
   hyperparameter_list = list("sign_size" = familiar:::get_n_features(good_data)),
   learner = "survival_regr_weibull")
-
-# Train the model using wide data.
-wide_model <- suppressWarnings(familiar:::test_train(
-  data = wide_data,
-  cluster_method = "none",
-  imputation_method = "simple",
-  hyperparameter_list = list("sign_size" = familiar:::get_n_features(wide_data)),
-  learner = "survival_regr_weibull"))
 
 testthat::test_that("Survival regression model trained correctly", {
   # Model trained
@@ -56,43 +50,18 @@ testthat::test_that("Survival regression model trained correctly", {
 testthat::test_that("Survival regression model has variable importance", {
   # Extract the variable importance table.
   vimp_table <- familiar:::get_vimp_table(good_model)
-
-  # Expect that the vimp table has three rows.
-  testthat::expect_equal(nrow(vimp_table), 3)
-
+  
+  # Expect that the vimp table has six rows.
+  testthat::expect_equal(nrow(vimp_table), 6L)
+  
   # Expect that the names are the same as that of the features.
-  testthat::expect_true(all(familiar:::get_feature_columns(good_data) %in% vimp_table$name))
-
-  # Expect that nodes has rank 1 and rx has rank 2.
-  testthat::expect_equal(vimp_table[rank == 1, ]$name, "nodes")
-  testthat::expect_equal(vimp_table[rank == 2, ]$name, "rx")
+  testthat::expect_true(
+    all(familiar:::get_feature_columns(good_data) %in% vimp_table$name))
+  
+  # Feature 1 is most important.
+  testthat::expect_equal(vimp_table[rank == 1, ]$name, "feature_1")
 })
 
-
-testthat::test_that("Survival regression model can train for wide data", {
-  # Model trained
-  testthat::expect_equal(familiar:::model_is_trained(wide_model), TRUE)
-
-  # With variable importance table.
-  testthat::expect_false(
-    familiar:::is_empty(familiar:::get_vimp_table(wide_model)))
-
-  # No valid predictions.
-  testthat::expect_true(familiar:::any_predictions_valid(
-    familiar:::.predict(wide_model, wide_data),
-    outcome_type = wide_data@outcome_type))
-
-  # No valid survival probability predictions.
-  testthat::expect_true(familiar:::any_predictions_valid(
-    familiar:::.predict(wide_model, wide_data, type = "survival_probability", time = 1000),
-    outcome_type = wide_data@outcome_type))
-
-  # Test that no deprecation warnings are given.
-  familiar:::test_not_deprecated(wide_model@messages$warning)
-
-  # Test that no errors appear.
-  testthat::expect_equal(wide_model@messages$error, NULL)
-})
 
 testthat::skip("Skip hyperparameter optimisation, unless manual.")
 
