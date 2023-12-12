0370069198f5b1f092b1fc7c00c946d6652168eb
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-12-12 17:42:31 +0100

    Risk stratification now works with prediction tables.

--- R/LearnerRiskStratification.R
+++ R/LearnerRiskStratification.R
@@ -1,6 +1,3 @@
-#' @include FamiliarS4Generics.R
-#' @include FamiliarS4Classes.R
-#' @include FamiliarDataComputationPredictionData.R
 #' @include PredictionTable.R
 
 .get_available_stratification_methods <- function() {
@@ -15,7 +12,7 @@
       ".find_survival_grouping_thresholds: only available for ",
       "survival outcome. Found: ", object@outcome_type))
   }
-
+  
   # Load settings to find survival thresholds
   settings <- get_settings()
 
@@ -32,7 +29,7 @@
 
   # Check if any predictions are valid.
   if (!any_predictions_valid(prediction_table)) return(NULL)
-
+  
   # Remove data with missing predictions.
   prediction_table <- remove_invalid_predictions(prediction_table)
   km_info_list <- list()
@@ -42,37 +39,38 @@
     if (cut_off_method == "median") {
       # Identify threshold
       cutoff <- .find_quantile_threshold(
-        prediction_table = prediction_table,
+        x = prediction_table,
         quantiles = 0.5
       )
       
     } else if (cut_off_method == "fixed") {
       # Identify thresholds
       cutoff <- .find_quantile_threshold(
-        prediction_table = prediction_table,
+        x = prediction_table,
         quantiles = settings$eval$strat_quant_threshold
       )
       
     } else if (cut_off_method == "optimised") {
       # Identify threshold
-      cutoff <- .find_maxstat_threshold(prediction_table = prediction_table)
+      cutoff <- .find_maxstat_threshold(x = prediction_table)
       
     } else if (cut_off_method %in% c("mean", "mean_winsor", "mean_trim")) {
       # Identify threshold
       cutoff <- .find_mean_threshold(
-        prediction_table = prediction_table,
+        x = prediction_table,
         method = cut_off_method
       )
       
     } else {
       ..error_reached_unreachable_code(paste0(
         ".find_survival_grouping_thresholds: encountered an unknown ",
-        "threshold type:", cut_off_method))
+        "threshold type:", cut_off_method
+      ))
     }
 
     # Find corresponding sizes of the generated groups
     risk_group <- .apply_risk_threshold(
-      prediction_table = prediction_table,
+      x = prediction_table,
       cutoff = cutoff
     )
     
@@ -100,105 +98,194 @@
 }
 
 
+# .find_mean_threshold ---------------------------------------------------------
+setGeneric(".find_mean_threshold", function(x, ...) standardGeneric(".find_mean_threshold"))
 
-.find_mean_threshold <- function(prediction_table, method) {
-  # Select finite values.
-  x <- prediction_table@prediction_data$predicted_outcome
+setMethod(
+  ".find_mean_threshold",
+  signature(x = "predictionTableSurvival"),
+  function(x, method, ...) {
+    data_slot <- ifelse(.is_merged_prediction_table(x), "data", "prediction_data")
+    values <- slot(x, data_slot)$predicted_outcome
+    
+    if (method == "mean") {
+      return(mean(values))
+      
+    } else if (method == "mean_trim") {
+      return(trim(values))
+      
+    } else if (method == "mean_winsor") {
+      return(winsor(values))
+      
+    } else {
+      ..error_reached_unreachable_code(paste0(
+        "The provided method for setting the mean risk stratification method was not recognised: ",
+        method
+      ))
+    }
+  }
+)
 
-  if (method == "mean_trim") {
-    x <- trim(x)
-  } else if (method == "mean_winsor") {
-    x <- winsor(x)
-  } else {
-    ..error_reached_unreachable_code(paste0(
-      "The provided method for setting the mean risk stratification method was not recognised: ",
-      method
+
+# .find_quantile_threshold -----------------------------------------------------
+setGeneric(".find_quantile_threshold", function(x, ...) standardGeneric(".find_quantile_threshold"))
+
+setMethod(
+  ".find_quantile_threshold",
+  signature(x = "predictionTableSurvival"),
+  function(x, quantiles, ...) {
+    
+    # Order quantiles in ascending order.
+    quantiles <- quantiles[order(quantiles)]
+    
+    # Extract values
+    data_slot <- ifelse(.is_merged_prediction_table(x), "data", "prediction_data")
+    values <- slot(x, data_slot)$predicted_outcome
+    
+    # Return threshold values
+    return(stats::quantile(
+      x = values,
+      probs = quantiles,
+      names = FALSE,
+      na.rm = TRUE
     ))
   }
+)
 
-  return(mean(x))
-}
-
-
-
-.find_quantile_threshold <- function(prediction_table, quantiles) {
-  
-  if (is(prediction_table, "predictionTableSurvivalTime")) {
+setMethod(
+  ".find_quantile_threshold",
+  signature(x = "predictionTableSurvivalTime"),
+  function(x, quantiles, ...) {
     # For time-like predictions, we should use the complements of the provided
     # quantiles.
     quantiles <- abs(1 - quantiles)
+    
+    # Pass to main method with updated quantiles.
+    return(callNextMethod(x = x, quantiles = quantiles))
   }
-  
-  # Order quantiles in ascending order.
-  quantiles <- quantiles[order(quantiles)]
-
-  # Return threshold values
-  return(stats::quantile(
-    x = prediction_table@prediction_data$predicted_outcome,
-    probs = quantiles,
-    names = FALSE,
-    na.rm = TRUE
-  ))
-}
+)
 
 
+# .find_maxstat_threshold ------------------------------------------------------
+setGeneric(".find_maxstat_threshold", function(x, ...) standardGeneric(".find_maxstat_threshold"))
 
-.find_maxstat_threshold <- function(prediction_table) {
-  # Check whether the model always predicted the same value
-  if (stats::var(prediction_table@prediction_data$predicted_outcome) == 0) {
-    return(prediction_table@prediction_data$predicted_outcome[1])
+setMethod(
+  ".find_maxstat_threshold",
+  signature(x = "predictionTableSurvival"),
+  function(x, ...) {
+    require_package(
+      x = "maxstat",
+      purpose = "to determine an optimal risk threshold"
+    )
+    
+    # Convert prediction_table to a data.table.
+    x <- .as_data_table(x)
+    
+    # Check for invariant features.
+    if (stats::var(x$predicted_outcome) == 0) {
+      return(x$predicted_outcome[1])
+    }
+    
+    # Perform maxstat test
+    h <- tryCatch(
+      maxstat::maxstat.test(
+        survival::Surv(outcome_time, outcome_event) ~ predicted_outcome,
+        data = x,
+        smethod = "LogRank",
+        minprop = 0.10,
+        maxprop = 0.90),
+      error = identity)
+    
+    # Check that maxstat.test did not produce an error.
+    if (inherits(h, "error")) {
+      return(mean(x$predicted_outcome, na.rm = TRUE))
+    }
+    
+    # Check if at least 4 unique values are present for the smoothing spline
+    if (length(h$cuts) < 4) {
+      return(unname(h$estimate))
+    }
+    
+    # Smoothed scores
+    spline_fit <- tryCatch(
+      stats::smooth.spline(x = h$cuts, y = h$stats)$fit,
+      error = identity)
+    
+    # Capture error.
+    if (inherits(spline_fit, "error")) {
+      return(unname(h$estimate))
+    }
+    
+    # Predict scores on a fine grid
+    x_sample_cuts <- seq(
+      from = min(h$cuts),
+      to = max(h$cuts),
+      length.out = 100
+    )
+    
+    test_scores <- stats::predict(
+      object = spline_fit, 
+      x = x_sample_cuts
+    )$y
+    
+    return(x_sample_cuts[which.max(test_scores)])
   }
+)
 
-  require_package(
-    x = "maxstat",
-    purpose = "to determine an optimal risk threshold"
-  )
 
-  # Convert prediction_table to a data.table.
-  prediction_table <- .as_data_table(prediction_table)
-  
-  # Perform maxstat test
-  h <- tryCatch(
-    maxstat::maxstat.test(
-      survival::Surv(outcome_time, outcome_event) ~ predicted_outcome,
-      data = prediction_table,
-      smethod = "LogRank",
-      minprop = 0.10,
-      maxprop = 0.90),
-    error = identity)
+# .apply_risk_threshold --------------------------------------------------------
+setGeneric(".apply_risk_threshold", function(x, ...) standardGeneric(".apply_risk_threshold"))
 
-  # Check that maxstat.test did not produce an error.
-  if (inherits(h, "error")) {
-    return(mean(prediction_table$predicted_outcome, na.rm = TRUE))
+setMethod(
+  ".apply_risk_threshold",
+  signature(x = "predictionTableSurvival"),
+  function(x, cutoff, invert = FALSE) {
+    
+    # Convert to data table and get the predicted values.
+    x <- .as_data_table(x)$predicted_outcome
+    
+    # Initialise risk group
+    risk_group <- rep.int(1, times = length(x))
+    
+    # Iterate over cutoffs and define risk groups
+    for (current_cutoff in cutoff) {
+      # We assume that risk groups go from 1 (low risk) to k (high risk), with
+      # k-1 being the number of provided cutoff values.
+      if (invert) {
+        risk_group <- risk_group + as.numeric(x < current_cutoff)
+      } else {
+        risk_group <- risk_group + as.numeric(x >= current_cutoff)
+      }
+    }
+    
+    # Convert to factor
+    risk_group <- .assign_risk_group_names(
+      risk_group = risk_group,
+      cutoff = cutoff
+    )
+    
+    # Replace non-finite predicted values by NA.
+    risk_group[!is.finite(x)] <- NA
+    
+    # Return risk groups
+    return(risk_group)
   }
+)
 
-  # Check if at least 4 unique values are present for the smoothing spline
-  if (length(h$cuts) < 4) {
-    return(unname(h$estimate))
+setMethod(
+  ".apply_risk_threshold",
+  signature(x = "predictionTableSurvivalTime"),
+  function(x, cutoff, invert = TRUE) {
+    # For prediction of survival times, pass to  
+    return(
+      callNextMethod(
+        x = x,
+        cutoff = cutoff,
+        invert = TRUE
+      )
+    )
   }
-
-  # Smoothed scores
-  spline_fit <- tryCatch(
-    stats::smooth.spline(x = h$cuts, y = h$stats)$fit,
-    error = identity)
-
-  # Capture error.
-  if (inherits(spline_fit, "error")) {
-    return(unname(h$estimate))
-  }
-
-  # Predict scores on a fine grid
-  x_sample_cuts <- seq(
-    from = min(h$cuts),
-    to = max(h$cuts),
-    length.out = 100)
-  
-  test_scores <- stats::predict(
-    object = spline_fit, 
-    x = x_sample_cuts)$y
-
-  return(x_sample_cuts[which.max(test_scores)])
-}
+)
 
 
 
@@ -217,37 +304,6 @@
   return(group_sizes)
 }
 
-
-
-.apply_risk_threshold <- function(prediction_table, cutoff) {
-  # Initialise risk group
-  risk_group <- rep.int(1, times = length(predicted_values))
-
-  # Determine inversion. We assume that risk groups go from 1 (low risk) to k
-  # (high risk), with k-1 being the number of provided cutoff values.
-  invert <- !get_prediction_type(object = object) %in% .get_available_risklike_prediction_types()
-
-  # Iterate over cutoffs and define risk groups
-  for (current_cutoff in cutoff) {
-    if (invert) {
-      risk_group <- risk_group + as.numeric(predicted_values < current_cutoff)
-    } else {
-      risk_group <- risk_group + as.numeric(predicted_values >= current_cutoff)
-    }
-  }
-
-  # Convert to factor
-  risk_group <- .assign_risk_group_names(
-    risk_group = risk_group,
-    cutoff = cutoff
-  )
-
-  # Replace non-finite predicted values by NA.
-  risk_group[!is.finite(predicted_values)] <- NA
-
-  # Return risk groups
-  return(risk_group)
-}
 
 
 
