8a418c33539baa1797d745259813e5ce44ea4e04
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2024-01-03 17:22:33 +0100

    WIP on export_model_performance.

--- tests/testthat/test-export_model_performance.R
+++ tests/testthat/test-export_model_performance.R
@@ -1,9 +1,22 @@
 # Don't perform any further tests on CRAN due to running time.
 testthat::skip_on_cran()
 
-debug_flag <- FALSE
+debug_flag <- TRUE
 
 # Default
+familiar:::test_export(
+  export_function = familiar:::export_model_performance,
+  data_element = "model_performance",
+  not_available_all_prospective = TRUE,
+  not_available_any_prospective = c("binomial", "multinomial", "survival"),
+  not_available_single_sample = c("binomial", "multinomial", "survival"),
+  outcome_type_available = c("continuous", "binomial", "multinomial", "survival"),
+  estimation_type = "point",
+  create_novelty_detector = FALSE,
+  use_prediction_table = TRUE,
+  debug = debug_flag
+)
+
 results <- familiar:::test_export_specific(
   export_function = familiar:::export_model_performance,
   data_element = "model_performance",
@@ -12,5 +25,6 @@ results <- familiar:::test_export_specific(
   not_available_single_sample = c("binomial", "multinomial", "survival"),
   outcome_type_available = c("continuous", "binomial", "multinomial", "survival"),
   create_novelty_detector = FALSE,
+  use_prediction_table = FALSE,
   debug = debug_flag
 )
