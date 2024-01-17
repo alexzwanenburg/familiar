7f795e9dfc9d71ea28665cbc6d6e46e86c288626
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2024-01-17 17:02:41 +0100

    Checked KNN unit test.

--- tests/testthat/test-learner_knn_S4.R
+++ tests/testthat/test-learner_knn_S4.R
@@ -1,27 +1,36 @@
 # First test if all selectable learners are also available
 familiar:::test_all_learners_available(
-  learners = familiar:::.get_available_knn_learners(show_general = TRUE))
+  learners = familiar:::.get_available_knn_learners(show_general = TRUE)
+)
 
 # Don't perform any further tests on CRAN due to time of running the complete test.
 testthat::skip_on_cran()
 
 familiar:::test_all_learners_train_predict_vimp(
   learners = familiar:::.get_available_knn_learners(
-    show_general = FALSE, show_default = TRUE),
+    show_general = FALSE,
+    show_default = TRUE
+  ),
   hyperparameter_list = list(
     "continuous" = list("k" = 3),
     "binomial" = list("k" = 3),
-    "multinomial" = list("k" = 3)),
-  has_vimp = FALSE)
+    "multinomial" = list("k" = 3)
+  ),
+  has_vimp = FALSE
+)
 
 familiar:::test_all_learners_parallel_train_predict_vimp(
   learners = familiar:::.get_available_knn_learners(
-    show_general = FALSE, show_default = TRUE),
+    show_general = FALSE,
+    show_default = TRUE
+  ),
   hyperparameter_list = list(
     "continuous" = list("k" = 3),
     "binomial" = list("k" = 3),
-    "multinomial" = list("k" = 3)),
-  has_vimp = FALSE)
+    "multinomial" = list("k" = 3)
+  ),
+  has_vimp = FALSE
+)
 
 # Continuous outcome tests------------------------------------------------------
 
@@ -35,12 +44,14 @@ good_model <- familiar:::test_train(
   imputation_method = "simple",
   hyperparameter_list = list(
     "sign_size" = familiar:::get_n_features(good_data),
-    "k" = 3),
-  learner = "k_nearest_neighbours_gower")
+    "k" = 3
+  ),
+  learner = "k_nearest_neighbours_gower"
+)
 
 testthat::test_that("k-nearest neighbour model trained correctly", {
   # Model trained
-  testthat::expect_equal(familiar:::model_is_trained(good_model), TRUE)
+  testthat::expect_true(familiar:::model_is_trained(good_model))
 
   # Check that no deprecation warnings are given.
   familiar:::test_not_deprecated(good_model@messages$warning)
@@ -54,7 +65,7 @@ testthat::test_that("k-nearest neighbour model has no variable importance", {
   vimp_table <- familiar:::get_vimp_table(good_model)
 
   # Expect that the vimp table is empty.
-  testthat::expect_equal(familiar:::is_empty(vimp_table), TRUE)
+  testthat::expect_true(familiar:::is_empty(vimp_table))
 })
 
 
@@ -70,12 +81,14 @@ good_model <- familiar:::test_train(
   imputation_method = "simple",
   hyperparameter_list = list(
     "sign_size" = familiar:::get_n_features(good_data),
-    "k" = 3),
-  learner = "k_nearest_neighbours_gower")
+    "k" = 3
+  ),
+  learner = "k_nearest_neighbours_gower"
+)
 
 testthat::test_that("k-nearest neighbour model trained correctly", {
   # Model trained
-  testthat::expect_equal(familiar:::model_is_trained(good_model), TRUE)
+  testthat::expect_true(familiar:::model_is_trained(good_model))
 
   # Check that no deprecation warnings are given.
   familiar:::test_not_deprecated(good_model@messages$warning)
@@ -89,7 +102,7 @@ testthat::test_that("k-nearest neighbour model does not have variable importance
   vimp_table <- familiar:::get_vimp_table(good_model)
 
   # Expect that the vimp table has no rows.
-  testthat::expect_equal(familiar:::is_empty(vimp_table), TRUE)
+  testthat::expect_true(familiar:::is_empty(vimp_table))
 })
 
 
@@ -105,12 +118,14 @@ good_model <- suppressWarnings(familiar:::test_train(
   imputation_method = "simple",
   hyperparameter_list = list(
     "sign_size" = familiar:::get_n_features(good_data),
-    "k" = 3),
-  learner = "k_nearest_neighbours_gower"))
+    "k" = 3
+  ),
+  learner = "k_nearest_neighbours_gower"
+))
 
 testthat::test_that("k-nearest neighbour model trained correctly", {
   # Model trained
-  testthat::expect_equal(familiar:::model_is_trained(good_model), TRUE)
+  testthat::expect_true(familiar:::model_is_trained(good_model))
 
   # Check that no deprecation warnings are given.
   familiar:::test_not_deprecated(good_model@messages$warning)
@@ -124,7 +139,7 @@ testthat::test_that("k-nearest neighbour model has no variable importance", {
   vimp_table <- familiar:::get_vimp_table(good_model)
 
   # Expect that the vimp table is empty.
-  testthat::expect_equal(familiar:::is_empty(vimp_table), TRUE)
+  testthat::expect_true(familiar:::is_empty(vimp_table))
 })
 
 
@@ -132,6 +147,9 @@ testthat::skip("Skip hyperparameter optimisation, unless manual.")
 
 familiar:::test_hyperparameter_optimisation(
   learners = familiar:::.get_available_knn_learners(
-    show_general = TRUE, show_default = TRUE),
+    show_general = TRUE,
+    show_default = TRUE
+  ),
   debug = FALSE,
-  parallel = FALSE)
+  parallel = FALSE
+)
