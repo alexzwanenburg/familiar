d547cc2086006eb1851fbaee05acb64e65869304
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2024-01-17 15:07:01 +0100

    Updated cox learner unit test.

--- tests/testthat/test-learner_cox_s4.R
+++ tests/testthat/test-learner_cox_s4.R
@@ -7,10 +7,12 @@ testthat::skip_on_cran()
 
 # Generic test
 familiar:::test_all_learners_train_predict_vimp(
-  learners = familiar:::.get_available_cox_learners(show_general = TRUE))
+  learners = familiar:::.get_available_cox_learners(show_general = TRUE)
+)
 
 familiar:::test_all_learners_parallel_train_predict_vimp(
-  learners = familiar:::.get_available_cox_learners(show_general = TRUE))
+  learners = familiar:::.get_available_cox_learners(show_general = TRUE)
+)
 
 # Create test data sets.
 good_data <- familiar:::test_create_good_data("survival")
@@ -21,24 +23,15 @@ good_model <- familiar:::test_train(
   cluster_method = "none",
   imputation_method = "simple",
   hyperparameter_list = list("sign_size" = familiar:::get_n_features(good_data)),
-  learner = "cox")
+  learner = "cox"
+)
 
 
 testthat::test_that("Cox model trained correctly", {
   # Model trained
-  testthat::expect_equal(familiar:::model_is_trained(good_model), TRUE)
+  testthat::expect_true(familiar:::model_is_trained(good_model))
 
-  # Test the prediction type
-  testthat::expect_equal(
-    familiar:::get_prediction_type(good_model),
-    "hazard_ratio")
-
-  # Test that the model predicts hazard ratios
-  testthat::expect_equal(
-    familiar:::get_prediction_type(good_model, type = "survival_probability"),
-    "survival_probability")
-
-  # Checkt that no deprecation warnings are given.
+  # Check that no deprecation warnings are given.
   familiar:::test_not_deprecated(good_model@messages$warning)
 
   # Test that no errors appear.
@@ -55,12 +48,12 @@ testthat::test_that("Cox model has variable importance", {
   
   # Expect that the names are the same as that of the features.
   testthat::expect_true(
-    all(familiar:::get_feature_columns(good_data) %in% vimp_table$name))
+    all(familiar:::get_feature_columns(good_data) %in% vimp_table$name)
+  )
   
   # Feature 1 is most important.
   testthat::expect_equal(vimp_table[rank == 1, ]$name, "feature_1")
 })
-
 
 testthat::skip("Skip hyperparameter optimisation, unless manual.")
 
@@ -68,4 +61,5 @@ testthat::skip("Skip hyperparameter optimisation, unless manual.")
 familiar:::test_hyperparameter_optimisation(
   learners = familiar:::.get_available_cox_learners(show_general = TRUE),
   debug = FALSE,
-  parallel = FALSE)
+  parallel = FALSE
+)
