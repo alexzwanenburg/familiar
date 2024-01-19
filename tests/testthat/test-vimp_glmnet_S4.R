4a9c83b97f1b0e54f6c3f8b201cc403dba95d9ba
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2024-01-19 11:32:48 +0100

    Update glmnet VIMP unit test.

--- tests/testthat/test-vimp_glmnet_S4.R
+++ tests/testthat/test-vimp_glmnet_S4.R
@@ -1,18 +1,23 @@
 # First test if all selectable learners are also available
 familiar:::test_all_vimp_methods_available(
-  vimp_methods = familiar:::.get_available_glmnet_ridge_vimp_methods(show_general = TRUE))
+  vimp_methods = familiar:::.get_available_glmnet_ridge_vimp_methods(show_general = TRUE)
+)
 familiar:::test_all_vimp_methods_available(
-  vimp_methods = familiar:::.get_available_glmnet_lasso_vimp_methods(show_general = TRUE))
+  vimp_methods = familiar:::.get_available_glmnet_lasso_vimp_methods(show_general = TRUE)
+)
 familiar:::test_all_vimp_methods_available(
-  vimp_methods = familiar:::.get_available_glmnet_elastic_net_vimp_methods(show_general = TRUE))
+  vimp_methods = familiar:::.get_available_glmnet_elastic_net_vimp_methods(show_general = TRUE)
+)
 
 # Don't perform any further tests on CRAN due to time of running the complete test.
 testthat::skip_on_cran()
 
 familiar:::test_all_vimp_methods(
-  vimp_methods = familiar:::.get_available_glmnet_ridge_vimp_methods(show_general = FALSE))
+  vimp_methods = familiar:::.get_available_glmnet_ridge_vimp_methods(show_general = FALSE)
+)
 familiar:::test_all_vimp_methods(
-  vimp_methods = familiar:::.get_available_glmnet_lasso_vimp_methods(show_general = FALSE))
+  vimp_methods = familiar:::.get_available_glmnet_lasso_vimp_methods(show_general = FALSE)
+)
 familiar:::test_all_vimp_methods(
   vimp_methods = familiar:::.get_available_glmnet_elastic_net_vimp_methods(show_general = FALSE),
   hyperparameter_list = list(
@@ -25,9 +30,11 @@ familiar:::test_all_vimp_methods(
 
 # Parallel tests
 familiar:::test_all_vimp_methods_parallel(
-  vimp_methods = familiar:::.get_available_glmnet_ridge_vimp_methods(show_general = FALSE))
+  vimp_methods = familiar:::.get_available_glmnet_ridge_vimp_methods(show_general = FALSE)
+)
 familiar:::test_all_vimp_methods_parallel(
-  vimp_methods = familiar:::.get_available_glmnet_lasso_vimp_methods(show_general = FALSE))
+  vimp_methods = familiar:::.get_available_glmnet_lasso_vimp_methods(show_general = FALSE)
+)
 familiar:::test_all_vimp_methods_parallel(
   vimp_methods = familiar:::.get_available_glmnet_elastic_net_vimp_methods(show_general = FALSE),
   hyperparameter_list = list(
@@ -43,14 +50,15 @@ testthat::skip("Skip hyperparameter optimisation, unless manual.")
 familiar:::test_hyperparameter_optimisation(
   vimp_methods = familiar:::.get_available_glmnet_ridge_vimp_methods(show_general = FALSE),
   debug = TRUE,
-  parallel = FALSE)
-
+  parallel = FALSE
+)
 familiar:::test_hyperparameter_optimisation(
   vimp_methods = familiar:::.get_available_glmnet_lasso_vimp_methods(show_general = FALSE),
   debug = TRUE,
-  parallel = FALSE)
-
+  parallel = FALSE
+)
 familiar:::test_hyperparameter_optimisation(
   vimp_methods = familiar:::.get_available_glmnet_elastic_net_vimp_methods(show_general = TRUE),
   debug = TRUE,
-  parallel = FALSE)
+  parallel = FALSE
+)
