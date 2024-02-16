4e820c0ce903163716aa4ba8e262dfc095e42ab2
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2024-02-16 15:56:53 +0100

    Update test-plot_kaplan_meier_curve.R

--- tests/testthat/test-plot_kaplan_meier_curve.R
+++ tests/testthat/test-plot_kaplan_meier_curve.R
@@ -27,6 +27,16 @@ familiar:::test_plot_ordering(
   plot_function = familiar:::plot_kaplan_meier,
   data_element = "risk_stratification_data",
   outcome_type_available = c("survival"),
+  use_prediction_table = TRUE,
+  prediction_type = list("survival" = "risk_stratification"),
+  debug = debug_flag
+)
+
+# Test alignment of different plots, with missing data.
+familiar:::test_plot_ordering(
+  plot_function = familiar:::plot_kaplan_meier,
+  data_element = "risk_stratification_data",
+  outcome_type_available = c("survival"),
   plot_args = list(
     "facet_by" = c("learner", "fs_method", "data_set"),
     "color_by" = "group"),
