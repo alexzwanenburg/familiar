71cd53e02049e203bf2610f8cf9d045a8f765df4
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2024-09-27 17:07:41 +0200

    Added internal methods for computing AUC values.

--- R/MetricS4AUC.R
+++ R/MetricS4AUC.R
@@ -75,32 +75,25 @@ setMethod(
       # Find the current positive and negative classes
       positive_class <- class_combinations[1L, ii]
       negative_class <- class_combinations[2L, ii]
-
-      # Get the probabilities that correspond to the positive and
-      # negative class in outcome (g and f in Hand et al.).
+      
+      # Get the probabilities that correspond to the positive and # negative
+      # class in outcome (g and f in Hand et al.).
       class_probability_positive <- data[outcome == positive_class, ][[positive_class]]
       class_probability_negative <- data[outcome == negative_class, ][[positive_class]]
-
-      # Get number of positive and negative class entries (n0 and n1 in Hand et
-      # al.).
-      n_positive <- length(class_probability_positive)
-      n_negative <- length(class_probability_negative)
-
-      # Calculate AUC only when positive and negative classes are present.
-      if (n_positive > 0L & n_negative > 0L) {
-        # Determine probability ranks
-        sample_rank <- data.table::frank(
-          x = c(class_probability_positive, class_probability_negative),
-          ties.method = "average"
-        )
-
-        # Calculate AUC
-        auc_score[ii] <- (sum(sample_rank[seq_len(n_positive)]) - n_positive * (n_positive + 1L) / 2.0) /
-          (n_positive * n_negative)
-        
-      } else {
-        auc_score[ii] <- NA_real_
-      }
+      
+      # Set x and y_obs
+      x <- c(class_probability_positive, class_probability_negative)
+      
+      y_obs <- c(
+        rep_len(TRUE, length(class_probability_positive)),
+        rep_len(FALSE, length(class_probability_negative))
+      )
+      
+      # Compute AUC value for the current combination.
+      auc_score[ii] <- ..compute_auc_roc(
+        x = x,
+        y_obs = y_obs
+      )
     }
 
     # Calculate mean AUC (eq. 7 from Hand et al.). This has no effect for
@@ -111,6 +104,77 @@ setMethod(
     return(auc_score)
   }
 )
+
+
+.compute_auc_roc <- function(x, y_obs) {
+  if (!is.factor(y_obs)) ..error("y_obs should be a categorical value")
+  # Called from functions other then compute_metric_score.
+  outcome_classes <- levels(y_obs)
+  
+  # Define class combinations (>1 in case of multinomial outcomes)
+  class_combinations <- utils::combn(outcome_classes, 2L)
+  n_class_combinations <- ncol(class_combinations)
+  
+  # Generate empty auc vector AUC of the ROC is calculated according to Hand,
+  # D.J, and Till, R.J. A simple generalisation of the area under the ROC
+  # curve for multiple class classification problems, Machine Learning 45
+  # 171-186 (2001)
+  auc_score <- vector(mode = "numeric", length = n_class_combinations)
+  
+  # Iterate over combinations
+  for (ii in seq_len(n_class_combinations)) {
+    # Find the current positive and negative classes
+    positive_class <- class_combinations[1L, ii]
+    negative_class <- class_combinations[2L, ii]
+    
+    # Get the probabilities that correspond to the positive and # negative
+    # class in outcome (g and f in Hand et al.).
+    x_positive <- x[y_obs == positive_class]
+    x_negative <- x[y_obs == negative_class]
+    
+    # Compute AUC value for the current combination.
+    auc_score[ii] <- ..compute_auc_roc(
+      x = c(x_positive, x_negative),
+      y_obs = c(
+        rep_len(TRUE, length(x_positive)),
+        rep_len(FALSE, length(x_negative))
+      )
+    )
+  }
+  
+  # Calculate mean AUC (eq. 7 from Hand et al.). This has no effect for
+  # binomial AUC.
+  if (!any(is.finite(auc_score))) return(NA_real_)
+  auc_score <- mean(auc_score, na.rm = TRUE)
+  
+  return(auc_score)
+}
+
+
+..compute_auc_roc <- function(x, y_obs) {
+  # Called directly from compute_metric_score
+  if (!is.logical(y_obs)) ..error("y_obs should contain only boolean values")
+  n_positive <- sum(y_obs)
+  n_negative <- length(y_obs) - n_positive
+  
+  if (n_positive > 0L & n_negative > 0L) {
+    
+    # Determine ranks
+    sample_rank <- data.table::frank(
+      x = x,
+      ties.method = "average"
+    )
+    
+    # Calculate AUC
+    auc <- (sum(sample_rank[seq_len(n_positive)]) - n_positive * (n_positive + 1L) / 2.0) /
+      (n_positive * n_negative)
+    
+  } else {
+    auc <- NA_real_
+  }
+  
+  return(auc)
+}
 
 
 
